c3ef0bc9298e3ebcac397bce343f74895efdd497
Dejan Mijić <dmijic@acm.org>
2020-03-05 19:19:34 -0500

    Draft library API (#3)

---
+++ src/main/scala/zio/redis/api/Sets.scala
@@ -0,0 +1,27 @@
+package zio.redis.api
+
+import zio.redis.Command
+import zio.redis.Input._
+import zio.redis.Output._
+
+trait Sets {
+  final val sadd        = Command("SADD", Tuple2(StringInput, NonEmptyList(ByteInput)), LongOutput)
+  final val scard       = Command("SCARD", StringInput, LongOutput)
+  final val sdiff       = Command("SDIFF", NonEmptyList(StringInput), ByteOutput)
+  final val sdiffstore  = Command("SDIFFSTORE", Tuple2(StringInput, NonEmptyList(StringInput)), LongOutput)
+  final val sinter      = Command("SINTER", NonEmptyList(StringInput), ByteOutput)
+  final val sinterstore = Command("SINTERSTORE", Tuple2(StringInput, NonEmptyList(StringInput)), LongOutput)
+  final val sismember   = Command("SISMEMBER", Tuple2(StringInput, ByteInput), BoolOutput)
+  final val smembers    = Command("SMEMBERS", StringInput, ByteOutput)
+  final val smove       = Command("SMOVE", Tuple3(StringInput, StringInput, ByteInput), BoolOutput)
+  final val spop        = Command("SPOP", Tuple2(StringInput, OptionalInput(LongInput)), ByteOutput)
+  final val srandmember = Command("SRANDMEMBER", Tuple2(StringInput, OptionalInput(LongInput)), ByteOutput)
+  final val srem        = Command("SREM", Tuple2(StringInput, NonEmptyList(ByteInput)), LongOutput)
+  final val sscan = Command(
+    "SSCAN",
+    Tuple4(LongInput, OptionalInput(MatchInput), OptionalInput(LongInput), OptionalInput(StringInput)),
+    ScanOutput
+  )
+  final val sunion      = Command("SUNION", NonEmptyList(StringInput), ByteOutput)
+  final val sunionstore = Command("SUNIONSTORE", Tuple2(StringInput, NonEmptyList(StringInput)), LongOutput)
+}
---
+++ src/main/scala/zio/redis/api/Strings.scala
@@ -0,0 +1,28 @@
+package zio.redis.api
+
+import zio.redis.Command
+import zio.redis.Input._
+import zio.redis.Output._
+
+trait Strings {
+  final val append      = Command("APPEND", Tuple2(StringInput, ByteInput), LongOutput)
+  final val bitcount    = Command("BITCOUNT", Tuple2(StringInput, OptionalInput(RangeInput)), LongOutput)
+  final val decr        = Command("DECR", StringInput, LongOutput)
+  final val decrby      = Command("DECRBY", Tuple2(StringInput, LongInput), LongOutput)
+  final val get         = Command("GET", StringInput, ByteOutput)
+  final val getbit      = Command("GETBIT", Tuple2(StringInput, LongInput), LongOutput)
+  final val getrange    = Command("GETRANGE", Tuple2(StringInput, RangeInput), ByteOutput)
+  final val getset      = Command("GETSET", Tuple2(StringInput, ByteInput), ByteOutput)
+  final val incr        = Command("INCR", StringInput, LongOutput)
+  final val incrby      = Command("INCRBY", Tuple2(StringInput, LongInput), LongOutput)
+  final val incrbyfloat = Command("INCRBYFLOAT", Tuple2(StringInput, DoubleInput), ByteOutput)
+  final val mget        = Command("MGET", NonEmptyList(StringInput), StreamOutput)
+  final val mset        = Command("MSET", NonEmptyList(Tuple2(StringInput, ByteInput)), UnitOutput)
+  final val msetnx      = Command("MSETNX", NonEmptyList(Tuple2(StringInput, ByteInput)), BoolOutput)
+  final val psetex      = Command("PSETEX", Tuple3(StringInput, DurationInput, ByteInput), UnitOutput)
+  final val setbit      = Command("SETBIT", Tuple3(StringInput, LongInput, ByteInput), ByteOutput)
+  final val setex       = Command("SETEX", Tuple3(StringInput, DurationInput, ByteInput), UnitOutput)
+  final val setnx       = Command("SETNX", Tuple2(StringInput, ByteInput), BoolOutput)
+  final val setrange    = Command("SETRANGE", Tuple3(StringInput, LongInput, ByteInput), LongOutput)
+  final val strlen      = Command("STRLEN", StringInput, LongOutput)
+}
