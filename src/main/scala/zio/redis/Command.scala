ae0d5c5e981da824e908992b7e6d6308325d0a98
Landlocked Surfer <landlocked.surfer@yahoo.com>
2020-03-28 21:56:42 +0100

    Add missing sorted sets commands (#17)

--- src/main/scala/zio/redis/Command.scala
+++ src/main/scala/zio/redis/Command.scala
@@ -23,11 +23,31 @@ object Command {
     def apply(a: A)(b: B, bs: B*): Out = command.run((a, (b, bs.toList)))
   }
 
+  implicit final class Arg1Varargs1[-A, -B, -C, +Out](private val command: Command[(A, (B, List[B]), C), Out])
+      extends AnyVal {
+    def apply(a: A)(b: B, bs: B*)(c: C): Out = command.run((a, (b, bs.toList), c))
+  }
+
+  implicit final class Arg1Varargs2[-A, -B, -C, -D, -E, +Out](
+    private val command: Command[(A, B, (C, List[C]), D, E), Out]
+  ) extends AnyVal {
+    def apply(a: A, b: B)(c: C, cs: C*)(d: D, e: E): Out = command.run((a, b, (c, cs.toList), d, e))
+  }
+
   implicit final class Arg2[-A, -B, +Out](private val command: Command[(A, B), Out]) extends AnyVal {
     def apply(a: A, b: B): Out = command.run((a, b))
   }
 
   implicit final class Arg3[-A, -B, -C, +Out](private val command: Command[(A, B, C), Out]) extends AnyVal {
     def apply(a: A, b: B, c: C): Out = command.run((a, b, c))
+  }
+
+  implicit final class Arg4[-A, -B, -C, -D, +Out](private val command: Command[(A, B, C, D), Out]) extends AnyVal {
+    def apply(a: A, b: B, c: C, d: D): Out = command.run((a, b, c, d))
+  }
+
+  implicit final class Arg5[-A, -B, -C, -D, -E, +Out](private val command: Command[(A, B, C, D, E), Out])
+      extends AnyVal {
+    def apply(a: A, b: B, c: C, d: D, e: E): Out = command.run((a, b, c, d, e))
   }
 }
--- src/main/scala/zio/redis/api/SortedSets.scala
+++ src/main/scala/zio/redis/api/SortedSets.scala
@@ -10,13 +10,12 @@ trait SortedSets {
 
   final val zadd = Command(
     "ZADD",
-    Tuple6(
+    Tuple5(
       StringInput,
-      OptionalInput(ZAddUpdateInput),
-      OptionalInput(ZAddChangedInput),
-      OptionalInput(ZAddIncrementInput),
-      LongInput,
-      NonEmptyList(StringInput)
+      OptionalInput(SortedSetUpdateInput),
+      OptionalInput(SortedSetChangedInput),
+      OptionalInput(SortedSetIncrementInput),
+      NonEmptyList(SortedSetMemberScoreInput)
     ),
     LongOutput
   )
@@ -24,10 +23,76 @@ trait SortedSets {
   final val zcard   = Command("ZCARD", StringInput, LongOutput)
   final val zcount  = Command("ZCOUNT", Tuple2(StringInput, RangeInput), LongOutput)
   final val zincrby = Command("ZINCRBY", Tuple3(StringInput, LongInput, ByteInput), LongOutput)
+
+  final val zlexcount = Command("ZLEXCOUNT", Tuple2(StringInput, SortedSetLexRangeInput), LongOutput)
+
+  final val zinterstore = Command(
+    "ZINTERSTORE",
+    Tuple4(
+      StringInput,
+      NonEmptyList(StringInput),
+      OptionalInput(NonEmptyList(DoubleInput)),
+      OptionalInput(SortedSetAggregateInput)
+    ),
+    LongOutput
+  )
+
+  final val zpopmax = Command("ZPOPMAX", Tuple2(StringInput, OptionalInput(LongInput)), StreamOutput)
+  final val zpopmin = Command("ZPOPMIN", Tuple2(StringInput, OptionalInput(LongInput)), StreamOutput)
+
   final val zrange =
-    Command("ZRANGE", Tuple3(StringInput, RangeInput, OptionalInput(ZRangeWithScoresInput)), StreamOutput)
-  final val zrank    = Command("ZRANK", Tuple2(StringInput, ByteInput), ByteOutput)
-  final val zrem     = Command("ZREM", Tuple2(StringInput, NonEmptyList(ByteInput)), LongOutput)
+    Command("ZRANGE", Tuple3(StringInput, RangeInput, OptionalInput(SortedSetWithScoresInput)), StreamOutput)
+
+  final val zrangebylex = Command(
+    "ZRANGEBYLEX",
+    Tuple3(
+      StringInput,
+      SortedSetLexRangeInput,
+      OptionalInput(SortedSetLimitInput)
+    ),
+    StreamOutput
+  )
+
+  final val zrangebyscore = Command(
+    "ZRANGEBYSCORE",
+    Tuple4(
+      StringInput,
+      SortedSetScoreRangeInput,
+      OptionalInput(SortedSetWithScoresInput),
+      OptionalInput(SortedSetLimitInput)
+    ),
+    StreamOutput
+  )
+
+  final val zrank = Command("ZRANK", Tuple2(StringInput, ByteInput), ByteOutput)
+  final val zrem  = Command("ZREM", Tuple2(StringInput, NonEmptyList(ByteInput)), LongOutput)
+
+  final val zremrangebylex   = Command("ZREMRANGEBYLEX", Tuple2(StringInput, SortedSetLexRangeInput), LongOutput)
+  final val zremrangebyrank  = Command("ZREMRANGEBYRANK", Tuple2(StringInput, RangeInput), LongOutput)
+  final val zremrangebyscore = Command("ZREMRANGEBYSCORE", Tuple2(StringInput, SortedSetScoreRangeInput), LongOutput)
+
+  final val zrevrangebylex = Command(
+    "ZREVRANGEBYLEX",
+    Tuple3(
+      StringInput,
+      SortedSetLexRangeInput,
+      OptionalInput(SortedSetLimitInput)
+    ),
+    StreamOutput
+  )
+  final val zrevrangebyscore = Command(
+    "ZREVRANGEBYSCORE",
+    Tuple4(
+      StringInput,
+      SortedSetScoreRangeInput,
+      OptionalInput(SortedSetWithScoresInput),
+      OptionalInput(SortedSetLimitInput)
+    ),
+    StreamOutput
+  )
+
+  final val revrange =
+    Command("ZREVRANGE", Tuple3(StringInput, RangeInput, OptionalInput(SortedSetWithScoresInput)), StreamOutput)
   final val zrevrank = Command("ZREVRANK", Tuple2(StringInput, ByteInput), LongOutput)
   final val zscan = Command(
     "ZSCAN",
@@ -35,4 +100,15 @@ trait SortedSets {
     ScanOutput
   )
   final val zscore = Command("ZSCORE", Tuple2(StringInput, ByteInput), LongOutput)
+
+  final val zunionstore = Command(
+    "ZUNIONSTORE",
+    Tuple4(
+      StringInput,
+      NonEmptyList(StringInput),
+      OptionalInput(NonEmptyList(DoubleInput)),
+      OptionalInput(SortedSetAggregateInput)
+    ),
+    LongOutput
+  )
 }
