38a03aa442f485ed8e9caf63e1ff64346cc87925
Dejan Mijic <dmijic@acm.org>
2020-05-10 01:31:16 +0200

    Polish connection API

--- src/main/scala/zio/redis/api/Connection.scala
+++ src/main/scala/zio/redis/api/Connection.scala
@@ -6,9 +6,8 @@ import zio.redis.Output._
 
 trait Connection {
   final val auth   = Command("AUTH", StringInput, UnitOutput)
-  final val echo   = Command("ECHO", StringInput, ByteOutput)
+  final val echo   = Command("ECHO", StringInput, StringOutput)
   final val ping   = Command("PING", Varargs(StringInput), ByteOutput)
-  final val quit   = Command("QUIT", UnitInput, UnitOutput)
   final val select = Command("SELECT", LongInput, UnitOutput)
   final val swapdb = Command("SWAPDB", Tuple2(LongInput, LongInput), UnitOutput)
 }
