6ea43e84e15aaf7b9ce0445b502762418c6970f9
Dejan Mijic <dmijic@acm.org>
2020-05-10 01:11:59 +0200

    Move varargs to the bottom

--- src/main/scala/zio/redis/Command.scala
+++ src/main/scala/zio/redis/Command.scala
@@ -19,21 +19,6 @@ object Command {
     def apply(a: A): Out = command.run(a)
   }
 
-  implicit final class Arg1Varargs[-A, -B, +Out](private val command: Command[(A, (B, List[B])), Out]) extends AnyVal {
-    def apply(a: A)(b: B, bs: B*): Out = command.run((a, (b, bs.toList)))
-  }
-
-  implicit final class Arg1Varargs1[-A, -B, -C, +Out](private val command: Command[(A, (B, List[B]), C), Out])
-      extends AnyVal {
-    def apply(a: A)(b: B, bs: B*)(c: C): Out = command.run((a, (b, bs.toList), c))
-  }
-
-  implicit final class Arg1Varargs2[-A, -B, -C, -D, -E, +Out](
-    private val command: Command[(A, B, (C, List[C]), D, E), Out]
-  ) extends AnyVal {
-    def apply(a: A, b: B)(c: C, cs: C*)(d: D, e: E): Out = command.run((a, b, (c, cs.toList), d, e))
-  }
-
   implicit final class Arg2[-A, -B, +Out](private val command: Command[(A, B), Out]) extends AnyVal {
     def apply(a: A, b: B): Out = command.run((a, b))
   }
@@ -56,5 +41,20 @@ object Command {
   ) extends AnyVal {
     def apply(a: A, b: B, c: C, d: D, e: E, f: F, g: G, h: H, i: I, j: J, k: K): Out =
       command.run((a, b, c, d, e, f, g, h, i, j, k))
+  }
+
+  implicit final class Arg1Varargs[-A, -B, +Out](private val command: Command[(A, (B, List[B])), Out]) extends AnyVal {
+    def apply(a: A)(b: B, bs: B*): Out = command.run((a, (b, bs.toList)))
+  }
+
+  implicit final class Arg1Varargs1[-A, -B, -C, +Out](private val command: Command[(A, (B, List[B]), C), Out])
+      extends AnyVal {
+    def apply(a: A)(b: B, bs: B*)(c: C): Out = command.run((a, (b, bs.toList), c))
+  }
+
+  implicit final class Arg1Varargs2[-A, -B, -C, -D, -E, +Out](
+    private val command: Command[(A, B, (C, List[C]), D, E), Out]
+  ) extends AnyVal {
+    def apply(a: A, b: B)(c: C, cs: C*)(d: D, e: E): Out = command.run((a, b, (c, cs.toList), d, e))
   }
 }
