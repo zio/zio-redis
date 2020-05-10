1c4fccc1c6f48753e88919b1147d84f87b99a894
Dejan Mijić <dmijic@acm.org>
2020-05-10 14:07:00 +0200

    Add support for Scala 2.11 (#39)

--- project/BuildHelper.scala
+++ project/BuildHelper.scala
@@ -4,64 +4,6 @@ import sbtbuildinfo._
 import BuildInfoKeys._
 
 object BuildHelper {
-  private val Scala212        = "2.12.10"
-  private val Scala213        = "2.13.1"
-  private val SilencerVersion = "1.6.0"
-
-  private val stdOptions = Seq(
-    "-encoding",
-    "UTF-8",
-    "-explaintypes",
-    "-Yrangepos",
-    "-feature",
-    "-language:higherKinds",
-    "-language:existentials",
-    "-Xlint:_,-type-parameter-shadow",
-    "-Xsource:2.13",
-    "-Ywarn-numeric-widen",
-    "-Ywarn-value-discard",
-    "-unchecked",
-    "-deprecation",
-    "-Xfatal-warnings"
-  )
-
-  private val stdOpts213 = Seq(
-    "-Wunused:imports",
-    "-Wvalue-discard",
-    "-Wunused:patvars",
-    "-Wunused:privates",
-    "-Wunused:params",
-    "-Wvalue-discard"
-  )
-
-  private val stdOptsUpto212 = Seq(
-    "-Xfuture",
-    "-Ypartial-unification",
-    "-Ywarn-nullary-override",
-    "-Yno-adapted-args",
-    "-Ywarn-infer-any",
-    "-Ywarn-inaccessible",
-    "-Ywarn-nullary-unit",
-    "-Ywarn-unused-import"
-  )
-
-  private def extraOptions(scalaVersion: String) =
-    CrossVersion.partialVersion(scalaVersion) match {
-      case Some((2, 13)) =>
-        stdOpts213
-      case Some((2, 12)) =>
-        Seq(
-          "-opt-warnings",
-          "-Ywarn-extra-implicit",
-          "-Ywarn-unused:_,imports",
-          "-Ywarn-unused:imports",
-          "-opt:l:inline",
-          "-opt-inline-from:<source>"
-        ) ++ stdOptsUpto212
-      case _             =>
-        Seq("-Xexperimental") ++ stdOptsUpto212
-    }
-
   def buildInfoSettings(packageName: String) =
     Seq(
       buildInfoKeys := Seq[BuildInfoKey](name, version, scalaVersion, sbtVersion, isSnapshot),
@@ -72,15 +14,80 @@ object BuildHelper {
   def stdSettings(prjName: String) =
     Seq(
       name := s"$prjName",
-      crossScalaVersions := Seq(Scala212, Scala213),
+      crossScalaVersions := Seq(Scala211, Scala212, Scala213),
       scalaVersion in ThisBuild := Scala212,
-      scalacOptions := stdOptions ++ extraOptions(scalaVersion.value),
+      scalacOptions := StdOpts ++ extraOptions(scalaVersion.value),
       libraryDependencies ++=
         Seq(
           "com.github.ghik" % "silencer-lib" % SilencerVersion % Provided cross CrossVersion.full,
-          compilerPlugin("com.github.ghik" % "silencer-plugin" % SilencerVersion cross CrossVersion.full),
-          compilerPlugin("org.typelevel"  %% "kind-projector"  % "0.10.3")
+          compilerPlugin("com.github.ghik" % "silencer-plugin" % SilencerVersion cross CrossVersion.full)
         ),
       incOptions ~= (_.withLogRecompileOnMacro(false))
     )
+
+  private val Scala211        = "2.11.12"
+  private val Scala212        = "2.12.10"
+  private val Scala213        = "2.13.1"
+  private val SilencerVersion = "1.4.4"
+
+  private val StdOpts =
+    Seq(
+      "-deprecation",
+      "-encoding",
+      "UTF-8",
+      "-feature",
+      "-unchecked",
+      "-language:higherKinds",
+      "-language:existentials",
+      "-explaintypes",
+      "-Yrangepos",
+      "-Xlint:_,-missing-interpolator,-type-parameter-shadow",
+      "-Ywarn-dead-code",
+      "-Ywarn-numeric-widen",
+      "-Ywarn-value-discard",
+      "-Xfatal-warnings"
+    )
+
+  private def extraOptions(scalaVersion: String): Seq[String] =
+    CrossVersion.partialVersion(scalaVersion) match {
+      case Some((2, 13)) =>
+        Seq(
+          "-Wunused:imports",
+          "-Wvalue-discard",
+          "-Wunused:patvars",
+          "-Wunused:privates",
+          "-Wunused:params",
+          "-Wvalue-discard"
+        )
+
+      case Some((2, 12)) =>
+        Seq(
+          "-Xfuture",
+          "-Ypartial-unification",
+          "-Ywarn-nullary-override",
+          "-Yno-adapted-args",
+          "-Ywarn-infer-any",
+          "-Ywarn-inaccessible",
+          "-Ywarn-nullary-unit",
+          "-Ywarn-unused-import",
+          "-Ywarn-extra-implicit",
+          "-Ywarn-unused:_,imports",
+          "-Ywarn-unused:imports"
+        )
+
+      case Some((2, 11)) =>
+        Seq(
+          "-Xexperimental",
+          "-Xfuture",
+          "-Ypartial-unification",
+          "-Ywarn-nullary-override",
+          "-Yno-adapted-args",
+          "-Ywarn-infer-any",
+          "-Ywarn-inaccessible",
+          "-Ywarn-nullary-unit",
+          "-Ywarn-unused-import"
+        )
+
+      case _ => Seq.empty
+    }
 }
