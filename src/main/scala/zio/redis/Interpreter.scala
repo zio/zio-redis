7674cfe58fe2e965ffacd7f7707559f5232d8998
Dejan Mijic <dmijic@acm.org>
2020-05-10 14:14:27 +0200

    Draft interpreter module

---
+++ src/main/scala/zio/redis/Interpreter.scala
@@ -0,0 +1,19 @@
+package zio.redis
+
+import java.io.IOException
+
+import zio.{ Chunk, Has, IO, Layer, ULayer }
+
+trait Interpreter {
+  type RedisExecutor = Has[RedisExecutor.Service]
+
+  object RedisExecutor {
+    trait Service {
+      def execute(input: Chunk[Byte]): IO[RedisError, Byte]
+    }
+
+    def live(port: Int, host: String): Layer[IOException, RedisExecutor] = ???
+
+    def test: ULayer[RedisExecutor] = ???
+  }
+}
--- src/main/scala/zio/redis/package.scala
+++ src/main/scala/zio/redis/package.scala
@@ -16,3 +16,4 @@ package object redis
     with options.Shared
     with options.SortedSets
     with options.Strings
+    with Interpreter
