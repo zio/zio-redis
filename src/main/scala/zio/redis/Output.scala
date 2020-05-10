bc20d1f3472712f807dd1a04fdeedd7306bca7be
Dejan Mijic <dmijic@acm.org>
2020-05-10 02:37:45 +0200

    Remove effects from output

--- src/main/scala/zio/redis/Output.scala
+++ src/main/scala/zio/redis/Output.scala
@@ -1,6 +1,6 @@
 package zio.redis
 
-import zio.{ Chunk, IO }
+import zio.Chunk
 import zio.duration.Duration
 
 sealed trait Output[+A]
@@ -8,14 +8,14 @@ sealed trait Output[+A]
 object Output {
   type Bytes = Chunk[Byte]
 
-  case object BoolOutput                                 extends Output[IO[Error, Boolean]]
-  case object ByteOutput                                 extends Output[IO[Error, Bytes]]
-  case object ChunkOutput                                extends Output[IO[Error, Chunk[Bytes]]]
-  case object DoubleOutput                               extends Output[IO[Error, Double]]
-  case object DurationOutput                             extends Output[IO[Error, Duration]]
-  case object LongOutput                                 extends Output[IO[Error, Long]]
-  case object ScanOutput                                 extends Output[IO[Error, (Long, Chunk[Bytes])]]
-  case object StringOutput                               extends Output[IO[Error, String]]
-  case object UnitOutput                                 extends Output[IO[Error, Unit]]
-  final case class OptionalOutput[+A](output: Output[A]) extends Output[IO[Error, Option[A]]]
+  case object BoolOutput                                 extends Output[Boolean]
+  case object ByteOutput                                 extends Output[Bytes]
+  case object ChunkOutput                                extends Output[Chunk[Bytes]]
+  case object DoubleOutput                               extends Output[Double]
+  case object DurationOutput                             extends Output[Duration]
+  case object LongOutput                                 extends Output[Long]
+  case object ScanOutput                                 extends Output[(Long, Chunk[Bytes])]
+  case object StringOutput                               extends Output[String]
+  case object UnitOutput                                 extends Output[Unit]
+  final case class OptionalOutput[+A](output: Output[A]) extends Output[Option[A]]
 }
