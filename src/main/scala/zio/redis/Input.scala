008d7842c22442eb0666fa1929ca51ed6fbf794b
Dejan Mijic <dmijic@acm.org>
2020-05-25 00:07:06 +0200

    Move byte chunks to transport level

--- src/main/scala/zio/redis/Input.scala
+++ src/main/scala/zio/redis/Input.scala
@@ -1,5 +1,6 @@
 package zio.redis
 
+// import java.nio.charset.StandardCharsets.UTF_8
 import java.time.Instant
 
 import zio.Chunk
@@ -8,154 +9,162 @@ import zio.duration.Duration
 import scala.util.matching.Regex
 
 sealed trait Input[-A] {
-  private[redis] def encode(data: A): Chunk[Byte]
+  private[redis] def encode(data: A): Chunk[String]
 }
 
 object Input {
+  // private[this] def wrap(text: String): String = s"$$${text.length}\r\n$text\r\n"
+
+  // def chunk(args: Chunk[String]): Chunk[Byte] = {
+  //   val wrapped  = args.map(wrap).mkString
+  //   val envelope = s"*${args.length}\r\n$wrapped"
+  //   Chunk.fromArray(envelope.getBytes(UTF_8))
+  // }
+
   case object AggregateInput extends Input[Aggregate] {
-    def encode(data: Aggregate): Chunk[Byte] = ???
+    def encode(data: Aggregate): Chunk[String] = ???
   }
 
   case object BoolInput extends Input[Boolean] {
-    def encode(data: Boolean): Chunk[Byte] = ???
+    def encode(data: Boolean): Chunk[String] = ???
   }
 
   case object BitFieldGetInput extends Input[BitFieldGet] {
-    def encode(data: BitFieldGet): Chunk[Byte] = ???
+    def encode(data: BitFieldGet): Chunk[String] = ???
   }
 
   case object BitFieldSetInput extends Input[BitFieldSet] {
-    def encode(data: BitFieldSet): Chunk[Byte] = ???
+    def encode(data: BitFieldSet): Chunk[String] = ???
   }
 
   case object BitFieldIncrInput extends Input[BitFieldIncr] {
-    def encode(data: BitFieldIncr): Chunk[Byte] = ???
+    def encode(data: BitFieldIncr): Chunk[String] = ???
   }
 
   case object BitFieldOverflowInput extends Input[BitFieldOverflow] {
-    def encode(data: BitFieldOverflow): Chunk[Byte] = ???
+    def encode(data: BitFieldOverflow): Chunk[String] = ???
   }
 
   case object BitOperationInput extends Input[BitOperation] {
-    def encode(data: BitOperation): Chunk[Byte] = ???
+    def encode(data: BitOperation): Chunk[String] = ???
   }
 
   case object BitPosRangeInput extends Input[BitPosRange] {
-    def encode(data: BitPosRange): Chunk[Byte] = ???
+    def encode(data: BitPosRange): Chunk[String] = ???
   }
 
   case object ByteInput extends Input[Chunk[Byte]] {
-    def encode(data: Chunk[Byte]): Chunk[Byte] = ???
+    def encode(data: Chunk[Byte]): Chunk[String] = ???
   }
 
   case object ChangedInput extends Input[Changed] {
-    def encode(data: Changed): Chunk[Byte] = ???
+    def encode(data: Changed): Chunk[String] = ???
   }
 
   case object CountInput extends Input[Count] {
-    def encode(data: Count): Chunk[Byte] = ???
+    def encode(data: Count): Chunk[String] = ???
   }
 
   case object DoubleInput extends Input[Double] {
-    def encode(data: Double): Chunk[Byte] = ???
+    def encode(data: Double): Chunk[String] = ???
   }
 
   case object DurationInput extends Input[Duration] {
-    def encode(data: Duration): Chunk[Byte] = ???
+    def encode(data: Duration): Chunk[String] = ???
   }
 
   case object IncrementInput extends Input[Increment] {
-    def encode(data: Increment): Chunk[Byte] = ???
+    def encode(data: Increment): Chunk[String] = ???
   }
 
   case object KeepTtlInput extends Input[KeepTtl] {
-    def encode(data: KeepTtl): Chunk[Byte] = ???
+    def encode(data: KeepTtl): Chunk[String] = ???
   }
 
   case object LexRangeInput extends Input[LexRange] {
-    def encode(data: LexRange): Chunk[Byte] = ???
+    def encode(data: LexRange): Chunk[String] = ???
   }
 
   case object LimitInput extends Input[Limit] {
-    def encode(data: Limit): Chunk[Byte] = ???
+    def encode(data: Limit): Chunk[String] = ???
   }
 
   case object LongInput extends Input[Long] {
-    def encode(data: Long): Chunk[Byte] = ???
+    def encode(data: Long): Chunk[String] = ???
   }
 
   case object LongLatInput extends Input[LongLat] {
-    def encode(data: LongLat): Chunk[Byte] = ???
+    def encode(data: LongLat): Chunk[String] = ???
   }
 
   case object MemberScoreInput extends Input[MemberScore] {
-    def encode(data: MemberScore): Chunk[Byte] = ???
+    def encode(data: MemberScore): Chunk[String] = ???
   }
 
   case object NoInput extends Input[Unit] {
-    def encode(data: Unit): Chunk[Byte] = ???
+    def encode(data: Unit): Chunk[String] = ???
   }
 
   final case class NonEmptyList[-A](a: Input[A]) extends Input[(A, List[A])] {
-    def encode(data: (A, List[A])): Chunk[Byte] = ???
+    def encode(data: (A, List[A])): Chunk[String] = ???
   }
 
   case object OrderInput extends Input[Order] {
-    def encode(data: Order): Chunk[Byte] = ???
+    def encode(data: Order): Chunk[String] = ???
   }
 
   case object RadiusUnitInput extends Input[RadiusUnit] {
-    def encode(data: RadiusUnit): Chunk[Byte] = ???
+    def encode(data: RadiusUnit): Chunk[String] = ???
   }
 
   case object RangeInput extends Input[Range] {
-    def encode(data: Range): Chunk[Byte] = ???
+    def encode(data: Range): Chunk[String] = ???
   }
 
   case object RegexInput extends Input[Regex] {
-    def encode(data: Regex): Chunk[Byte] = ???
+    def encode(data: Regex): Chunk[String] = ???
   }
 
   case object StoreDistInput extends Input[StoreDist] {
-    def encode(data: StoreDist): Chunk[Byte] = ???
+    def encode(data: StoreDist): Chunk[String] = ???
   }
 
   case object StoreInput extends Input[Store] {
-    def encode(data: Store): Chunk[Byte] = ???
+    def encode(data: Store): Chunk[String] = ???
   }
 
   case object ScoreRangeInput extends Input[ScoreRange] {
-    def encode(data: ScoreRange): Chunk[Byte] = ???
+    def encode(data: ScoreRange): Chunk[String] = ???
   }
 
   case object StringInput extends Input[String] {
-    def encode(data: String): Chunk[Byte] = ???
+    def encode(data: String): Chunk[String] = ???
   }
 
   final case class OptionalInput[-A](a: Input[A]) extends Input[Option[A]] {
-    def encode(data: Option[A]): Chunk[Byte] = ???
+    def encode(data: Option[A]): Chunk[String] = ???
   }
 
   case object TimeInput extends Input[Instant] {
-    def encode(data: Instant): Chunk[Byte] = ???
+    def encode(data: Instant): Chunk[String] = ???
   }
 
   final case class Tuple2[-A, -B](_1: Input[A], _2: Input[B]) extends Input[(A, B)] {
-    def encode(data: (A, B)): Chunk[Byte] = ???
+    def encode(data: (A, B)): Chunk[String] = ???
   }
 
   final case class Tuple3[-A, -B, -C](_1: Input[A], _2: Input[B], _3: Input[C]) extends Input[(A, B, C)] {
-    def encode(data: (A, B, C)): Chunk[Byte] = ???
+    def encode(data: (A, B, C)): Chunk[String] = ???
   }
 
   final case class Tuple4[-A, -B, -C, -D](_1: Input[A], _2: Input[B], _3: Input[C], _4: Input[D])
       extends Input[(A, B, C, D)] {
-    def encode(data: (A, B, C, D)): Chunk[Byte] = ???
+    def encode(data: (A, B, C, D)): Chunk[String] = ???
   }
 
   final case class Tuple5[-A, -B, -C, -D, -E](_1: Input[A], _2: Input[B], _3: Input[C], _4: Input[D], _5: Input[E])
       extends Input[(A, B, C, D, E)] {
-    def encode(data: (A, B, C, D, E)): Chunk[Byte] = ???
+    def encode(data: (A, B, C, D, E)): Chunk[String] = ???
   }
 
   final case class Tuple7[-A, -B, -C, -D, -E, -F, -G](
@@ -193,30 +202,30 @@ object Input {
     _10: Input[J],
     _11: Input[K]
   ) extends Input[(A, B, C, D, E, F, G, H, I, J, K)] {
-    def encode(data: (A, B, C, D, E, F, G, H, I, J, K)): Chunk[Byte] = ???
+    def encode(data: (A, B, C, D, E, F, G, H, I, J, K)): Chunk[String] = ???
   }
 
   case object UpdateInput extends Input[Update] {
-    def encode(data: Update): Chunk[Byte] = ???
+    def encode(data: Update): Chunk[String] = ???
   }
 
   final case class Varargs[-A](value: Input[A]) extends Input[Iterable[A]] {
-    def encode(data: Iterable[A]): Chunk[Byte] = ???
+    def encode(data: Iterable[A]): Chunk[String] = ???
   }
 
   case object WithScoresInput extends Input[WithScores] {
-    def encode(data: WithScores): Chunk[Byte] = ???
+    def encode(data: WithScores): Chunk[String] = ???
   }
 
   case object WithCoordInput extends Input[WithCoord] {
-    def encode(data: WithCoord): Chunk[Byte] = ???
+    def encode(data: WithCoord): Chunk[String] = ???
   }
 
   case object WithDistInput extends Input[WithDist] {
-    def encode(data: WithDist): Chunk[Byte] = ???
+    def encode(data: WithDist): Chunk[String] = ???
   }
 
   case object WithHashInput extends Input[WithHash] {
-    def encode(data: WithHash): Chunk[Byte] = ???
+    def encode(data: WithHash): Chunk[String] = ???
   }
 }
--- src/main/scala/zio/redis/Output.scala
+++ src/main/scala/zio/redis/Output.scala
@@ -4,47 +4,47 @@ import zio.Chunk
 import zio.duration.Duration
 
 sealed trait Output[+A] {
-  private[redis] def decode(blob: Chunk[Byte]): Either[RedisError, A]
+  private[redis] def decode(text: String): Either[RedisError, A]
 }
 
 object Output {
   case object BoolOutput extends Output[Boolean] {
-    def decode(blob: Chunk[Byte]): Either[RedisError, Boolean] = ???
+    def decode(text: String): Either[RedisError, Boolean] = ???
   }
 
   case object ByteOutput extends Output[Chunk[Byte]] {
-    def decode(blob: Chunk[Byte]): Either[RedisError, Chunk[Byte]] = ???
+    def decode(text: String): Either[RedisError, Chunk[Byte]] = ???
   }
 
   case object ChunkOutput extends Output[Chunk[Chunk[Byte]]] {
-    def decode(blob: Chunk[Byte]): Either[RedisError, Chunk[Chunk[Byte]]] = ???
+    def decode(text: String): Either[RedisError, Chunk[Chunk[Byte]]] = ???
   }
 
   case object DoubleOutput extends Output[Double] {
-    def decode(blob: Chunk[Byte]): Either[RedisError, Double] = ???
+    def decode(text: String): Either[RedisError, Double] = ???
   }
 
   case object DurationOutput extends Output[Duration] {
-    def decode(blob: Chunk[Byte]): Either[RedisError, Duration] = ???
+    def decode(text: String): Either[RedisError, Duration] = ???
   }
 
   case object LongOutput extends Output[Long] {
-    def decode(blob: Chunk[Byte]): Either[RedisError, Long] = ???
+    def decode(text: String): Either[RedisError, Long] = ???
   }
 
   final case class OptionalOutput[+A](output: Output[A]) extends Output[Option[A]] {
-    def decode(blob: Chunk[Byte]): Either[RedisError, Option[A]] = ???
+    def decode(text: String): Either[RedisError, Option[A]] = ???
   }
 
   case object ScanOutput extends Output[(Long, Chunk[Chunk[Byte]])] {
-    def decode(blob: Chunk[Byte]): Either[RedisError, (Long, Chunk[Chunk[Byte]])] = ???
+    def decode(text: String): Either[RedisError, (Long, Chunk[Chunk[Byte]])] = ???
   }
 
   case object StringOutput extends Output[String] {
-    def decode(blob: Chunk[Byte]): Either[RedisError, String] = ???
+    def decode(text: String): Either[RedisError, String] = ???
   }
 
   case object UnitOutput extends Output[Unit] {
-    def decode(blob: Chunk[Byte]): Either[RedisError, Unit] = ???
+    def decode(text: String): Either[RedisError, Unit] = ???
   }
 }
