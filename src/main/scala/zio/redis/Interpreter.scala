f9749f0f11a791400c75f822ad9b342fda3997a7
Dejan Mijic <dmijic@acm.org>
2020-05-25 02:26:01 +0200

    Draft redis interpreter

--- src/main/scala/zio/redis/Interpreter.scala
+++ src/main/scala/zio/redis/Interpreter.scala
@@ -1,8 +1,12 @@
 package zio.redis
 
 import java.io.IOException
+import java.net.InetSocketAddress
+import java.nio.ByteBuffer
+import java.nio.charset.StandardCharsets.UTF_8
+import java.nio.channels.SocketChannel
 
-import zio.{ Chunk, Has, IO, Layer, ULayer }
+import zio._
 
 trait Interpreter {
   type RedisExecutor = Has[RedisExecutor.Service]
@@ -12,8 +16,57 @@ trait Interpreter {
       def execute(command: Chunk[String]): IO[RedisError, String]
     }
 
-    def live(port: Int, host: String): Layer[IOException, RedisExecutor] = ???
+    def live(host: String, port: Int): Layer[IOException, RedisExecutor] = {
+      import internal._
 
-    def test: ULayer[RedisExecutor] = ???
+      val connect = IO.effect(unsafeConnect(host, port)).refineToOrDie[IOException]
+
+      ZLayer.fromManaged {
+        for {
+          channel <- ZManaged.fromAutoCloseable(connect)
+          queue   <- Queue.bounded[Request](100).toManaged_
+          _       <- dequeue(queue, channel).forever.forkManaged
+        } yield new Service {
+          def execute(command: Chunk[String]): IO[RedisError, String] = enqueue(command, queue).flatMap(_.await)
+        }
+      }
+    }
+
+    private[this] object internal {
+      type Request = (Chunk[String], Promise[RedisError, String])
+
+      def dequeue(queue: Queue[Request], channel: SocketChannel): UIO[Any] =
+        queue.take.flatMap {
+          case (command, result) =>
+            val exchange =
+              IO.effect {
+                unsafeSend(command, channel)
+                unsafeReceive(channel)
+              }
+
+            result.completeWith(exchange.refineOrDie(RedisError.asProtocolError))
+        }
+
+      def enqueue(command: Chunk[String], queue: Queue[Request]): UIO[Promise[RedisError, String]] =
+        Promise.make[RedisError, String].flatMap(p => queue.offer((command, p)).as(p))
+
+      def unsafeConnect(host: String, port: Int): SocketChannel = SocketChannel.open(new InetSocketAddress(host, port))
+
+      def unsafeEncode(command: Chunk[String]): ByteBuffer = {
+        val data     = command.mkString
+        val envelope = s"*${command.length}\r\n$data"
+
+        ByteBuffer.wrap(envelope.getBytes(UTF_8))
+      }
+
+      def unsafeSend(command: Chunk[String], channel: SocketChannel): Unit = {
+        val data = unsafeEncode(command)
+        data.flip()
+        while (data.hasRemaining())
+          channel.write(data)
+      }
+
+      def unsafeReceive(channel: SocketChannel): String = ???
+    }
   }
 }
--- src/main/scala/zio/redis/RedisError.scala
+++ src/main/scala/zio/redis/RedisError.scala
@@ -3,6 +3,10 @@ package zio.redis
 sealed trait RedisError
 
 object RedisError {
-  final case class WrongType(message: String)     extends RedisError
   final case class ProtocolError(message: String) extends RedisError
+  final case class WrongType(message: String)     extends RedisError
+
+  val asProtocolError: PartialFunction[Throwable, RedisError] = {
+    case t: Throwable => ProtocolError(t.getMessage)
+  }
 }
