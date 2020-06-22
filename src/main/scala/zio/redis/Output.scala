e47b54fbca20f05249d82291b654ec73e62c01aa
Dejan Mijic <dmijic@acm.org>
2020-06-22 10:55:48 +0200

    Decode chunk

--- src/main/scala/zio/redis/Output.scala
+++ src/main/scala/zio/redis/Output.scala
@@ -22,7 +22,7 @@ object Output {
   import RedisError._
 
   case object BoolOutput extends Output[Boolean] {
-    def tryDecode(text: String): Either[RedisError, Boolean] =
+    protected def tryDecode(text: String): Either[RedisError, Boolean] =
       if (text == ":1\r\n")
         Right(true)
       else if (text == ":0\r\n")
@@ -32,20 +32,58 @@ object Output {
   }
 
   case object ChunkOutput extends Output[Chunk[String]] {
-    def tryDecode(text: String): Either[RedisError, Chunk[String]] = ???
+    protected def tryDecode(text: String): Either[RedisError, Chunk[String]] =
+      Either.cond(text.startsWith("*"), parse(text), ProtocolError(s"$text isn't an array."))
+
+    private[this] def parse(text: String): Chunk[String] = {
+      var pos = 1
+      var len = 0
+
+      while (text.charAt(pos) != '\r') {
+        len = len * 10 + text.charAt(pos) - '0'
+        pos += 1
+      }
+
+      if (len == 0) Chunk.empty
+      else {
+        val data = Array.ofDim[String](len)
+        var idx = 0
+  
+        while (idx < len) {
+          // skip to the first size character
+          pos += 3
+ 
+          var itemLen = 0
+
+          while (text.charAt(pos) != '\r') {
+            itemLen = itemLen * 10 + text.charAt(pos) - '0'
+            pos += 1
+          }
+  
+          // skip to the first payload char
+          pos += 2
+
+          data(idx) = text.substring(pos, pos + itemLen)
+          idx += 1
+          pos += itemLen
+        }
+  
+        Chunk.fromArray(data)
+      }
+    }
   }
 
   case object DoubleOutput extends Output[Double] {
-    def tryDecode(text: String): Either[RedisError, Double] = ???
+    protected def tryDecode(text: String): Either[RedisError, Double] = ???
   }
 
   case object DurationOutput extends Output[Duration] {
-    def tryDecode(text: String): Either[RedisError, Duration] = ???
+    protected def tryDecode(text: String): Either[RedisError, Duration] = ???
   }
 
   case object LongOutput extends Output[Long] {
-    def tryDecode(text: String): Either[RedisError, Long] =
-      Either.cond(text.startsWith(":"), parse(text), ProtocolError(s"$text isn't an integer."))
+    protected def tryDecode(text: String): Either[RedisError, Long] =
+      Either.cond(text.startsWith(":"), parse(text), ProtocolError(s"$text isn't a number."))
 
     private[this] def parse(text: String): Long = {
       var pos   = 1
@@ -61,16 +99,16 @@ object Output {
   }
 
   final case class OptionalOutput[+A](output: Output[A]) extends Output[Option[A]] {
-    def tryDecode(text: String): Either[RedisError, Option[A]] =
+    protected def tryDecode(text: String): Either[RedisError, Option[A]] =
       if (text.startsWith("$-1")) Right(None) else output.tryDecode(text).map(Some(_))
   }
 
   case object ScanOutput extends Output[(Long, Chunk[String])] {
-    def tryDecode(text: String): Either[RedisError, (Long, Chunk[String])] = ???
+    protected def tryDecode(text: String): Either[RedisError, (Long, Chunk[String])] = ???
   }
 
   case object StringOutput extends Output[String] {
-    def tryDecode(text: String): Either[RedisError, String] =
+    protected def tryDecode(text: String): Either[RedisError, String] =
       Either.cond(text.startsWith("$"), parse(text), ProtocolError(s"$text isn't a string."))
 
     private[this] def parse(text: String): String = {
@@ -90,7 +128,8 @@ object Output {
   }
 
   case object UnitOutput extends Output[Unit] {
-    def tryDecode(text: String): Either[RedisError, Unit] =
+    protected def tryDecode(text: String): Either[RedisError, Unit] =
       Either.cond(text == "+OK\r\n", (), ProtocolError(s"$text isn't unit."))
   }
+
 }
