27ba92fff8df32cbd2fc6d6b1f782d11cd05f6fc
aleksandarskrbic <skrbic.alexa@gmail.com>
2020-06-28 17:19:20 +0200

    Extarct ttl test into separate test suite

--- redis/src/test/scala/zio/redis/ApiSpec.scala
+++ redis/src/test/scala/zio/redis/ApiSpec.scala
@@ -111,52 +111,82 @@ object ApiSpec extends BaseSpec {
             restored <- get(key)
           } yield assert(restore)(isRight) && assert(restored)(isSome(equalTo(value)))
         } @@ ignore,
-        suite("expiration")(
-          testM("pExpire followed by pTtl") {
+        suite("ttl")(
+          testM("check ttl for existing key") {
             for {
               key   <- uuid
               value <- uuid
               _     <- set(key, value, None, None, None)
-              exp   <- pExpire(key, 100.millis)
-              ttl   <- pTtl(key)
-            } yield assert(exp)(isTrue) && assert(ttl.toMillis)(isPositive)
+              _     <- pExpire(key, 1000.millis)
+              ttl   <- ttl(key).either
+            } yield assert(ttl)(isRight)
           },
-          testM("pExpireAt followed by exists and pTtl") {
+          testM("check ttl for non-existing key") {
+            for {
+              key <- uuid
+              ttl <- ttl(key).either
+            } yield assert(ttl)(isLeft)
+          },
+          testM("check pTtl for existing key") {
+            for {
+              key   <- uuid
+              value <- uuid
+              _     <- set(key, value, None, None, None)
+              _     <- pExpire(key, 1000.millis)
+              pTtl  <- pTtl(key).either
+            } yield assert(pTtl)(isRight)
+          },
+          testM("check pTtl for non-existing key") {
+            for {
+              key  <- uuid
+              pTtl <- pTtl(key).either
+            } yield assert(pTtl)(isLeft)
+          }
+        ),
+        suite("expiration")(
+          testM("set key expiration with pExpire command") {
             for {
               key       <- uuid
               value     <- uuid
-              expiresAt <- instantOf(1000)
               _         <- set(key, value, None, None, None)
-              _         <- pExpireAt(key, expiresAt)
+              exp       <- pExpire(key, 100.millis)
               response1 <- exists(key, Nil)
-              ttl       <- pTtl(key)
-              _         <- ZIO.sleep(1010.millis)
+              _         <- ZIO.sleep(110.millis)
               response2 <- exists(key, Nil)
-            } yield assert(response1)(isTrue) && assert(response2)(isFalse) && assert(ttl.toMillis)(isPositive)
+            } yield assert(exp)(isTrue) && assert(response1)(isTrue) && assert(response2)(isFalse)
           },
-          testM("expire followed by ttl and persist") {
-            for {
-              key       <- uuid
-              value     <- uuid
-              _         <- set(key, value, None, None, None)
-              _         <- expire(key, 10000.millis)
-              ttl1      <- ttl(key)
-              persisted <- persist(key)
-              ttl2      <- ttl(key).either
-            } yield assert(ttl1.toMillis)(isPositive) && assert(persisted)(isTrue) && assert(ttl2)(isLeft)
-          },
-          testM("expireAt followed by get and ttl") {
+          testM("set key expiration with pExpireAt command") {
             for {
               key       <- uuid
               value     <- uuid
               expiresAt <- instantOf(2000)
               _         <- set(key, value, None, None, None)
-              _         <- expireAt(key, expiresAt)
+              exp       <- pExpireAt(key, expiresAt)
               response1 <- exists(key, Nil)
-              ttl       <- ttl(key)
               _         <- ZIO.sleep(2010.millis)
               response2 <- exists(key, Nil)
-            } yield assert(response1)(isTrue) && assert(response2)(isFalse) && assert(ttl.toMillis)(isPositive)
+            } yield assert(exp)(isTrue) && assert(response1)(isTrue) && assert(response2)(isFalse)
+          },
+          testM("expire followed by persist") {
+            for {
+              key       <- uuid
+              value     <- uuid
+              _         <- set(key, value, None, None, None)
+              exp       <- expire(key, 10000.millis)
+              persisted <- persist(key)
+            } yield assert(exp)(isTrue) && assert(persisted)(isTrue)
+          },
+          testM("set key expiration with expireAt command") {
+            for {
+              key       <- uuid
+              value     <- uuid
+              expiresAt <- instantOf(2000)
+              _         <- set(key, value, None, None, None)
+              exp       <- expireAt(key, expiresAt)
+              response1 <- exists(key, Nil)
+              _         <- ZIO.sleep(2010.millis)
+              response2 <- exists(key, Nil)
+            } yield assert(exp)(isTrue) && assert(response1)(isTrue) && assert(response2)(isFalse)
           }
         ),
         suite("renaming")(
@@ -187,7 +217,7 @@ object ApiSpec extends BaseSpec {
               v       <- get(newKey)
             } yield assert(renamed)(isTrue) && assert(v)(isSome(equalTo(value)))
           },
-          testM("try to rename non-existing key with RENAMENX command") {
+          testM("try to rename non-existing key with renameNx command") {
             for {
               key     <- uuid
               newKey  <- uuid
@@ -202,7 +232,7 @@ object ApiSpec extends BaseSpec {
               value  <- uuid
               _      <- set(key, value, None, None, None)
               string <- typeOf(key)
-            } yield assert(string)(isSubtype[RedisType.String.type](anything))
+            } yield assert(string)(equalTo(RedisType.String))
           },
           testM("list type") {
             for {
@@ -210,7 +240,7 @@ object ApiSpec extends BaseSpec {
               value <- uuid
               _     <- lPush(key)(value)
               list  <- typeOf(key)
-            } yield assert(list)(isSubtype[RedisType.List.type](anything))
+            } yield assert(list)(equalTo(RedisType.List))
           },
           testM("set type") {
             for {
@@ -218,7 +248,7 @@ object ApiSpec extends BaseSpec {
               value <- uuid
               _     <- sAdd(key)(value)
               set   <- typeOf(key)
-            } yield assert(set)(isSubtype[RedisType.Set.type](anything))
+            } yield assert(set)(equalTo(RedisType.Set))
           },
           testM("sorted set type") {
             for {
@@ -226,7 +256,7 @@ object ApiSpec extends BaseSpec {
               value <- uuid
               _     <- zAdd(key, None, None, None, (MemberScore(1d, value), Nil))
               zset  <- typeOf(key)
-            } yield assert(zset)(isSubtype[RedisType.SortedSet.type](anything))
+            } yield assert(zset)(equalTo(RedisType.SortedSet))
           },
           testM("hash type") {
             for {
@@ -235,7 +265,7 @@ object ApiSpec extends BaseSpec {
               value <- uuid
               _     <- hSet(key)((field, value))
               hash  <- typeOf(key)
-            } yield assert(hash)(isSubtype[RedisType.Hash.type](anything))
+            } yield assert(hash)(equalTo(RedisType.Hash))
           }
         )
       )
