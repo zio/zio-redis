0e55a1609e4a9554332df0ad76795e0bf65df557
geoffjohn11 <geoffjohn11@gmail.com>
2020-07-18 09:23:36 -0400

    Adding hyperloglog tests (#90)

--- redis/src/test/scala/zio/redis/ApiSpec.scala
+++ redis/src/test/scala/zio/redis/ApiSpec.scala
@@ -3,13 +3,14 @@ package zio.redis
 import zio.test._
 import zio.clock.Clock
 
-object ApiSpec extends KeysSpec with ListSpec with GeoSpec {
+object ApiSpec extends KeysSpec with ListSpec with GeoSpec with HyperLogLogSpec {
 
   def spec =
     suite("Redis commands")(
       keysSuite,
       listSuite,
-      geoSuite
+      geoSuite,
+      hyperLogLogSuite
     ).provideCustomLayerShared(Executor ++ Clock.live)
 
   private val Executor = RedisExecutor.live("127.0.0.1", 6379).orDie
