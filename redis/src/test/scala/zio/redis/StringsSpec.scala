2ce139bd1b797d9a056e7048f8828e60b360aad5
Aleksandar Novakovic <anovakovic01@gmail.com>
2020-08-01 14:42:29 +0200

    Add tests for bitCount command
    Signed-off-by: Aleksandar Novakovic <anovakovic01@gmail.com>

--- redis/src/test/scala/zio/redis/StringsSpec.scala
+++ redis/src/test/scala/zio/redis/StringsSpec.scala
@@ -41,6 +41,69 @@ trait StringsSpec extends BaseSpec {
             len <- append(key, "b").either
           } yield assert(len)(isLeft(isSubtype[WrongType](anything)))
         }
+      ),
+      suite("bitCount")(
+        testM("over non-empty string") {
+          for {
+            key   <- uuid
+            _     <- set(key, "value", None, None, None)
+            count <- bitCount(key, None)
+          } yield assert(count)(equalTo(21L))
+        },
+        testM("over empty string") {
+          for {
+            key   <- uuid
+            count <- bitCount(key, None)
+          } yield assert(count)(equalTo(0L))
+        },
+        testM("error when not string") {
+          for {
+            key   <- uuid
+            _     <- sAdd(key)("a")
+            count <- bitCount(key, None).either
+          } yield assert(count)(isLeft(isSubtype[WrongType](anything)))
+        },
+        testM("over non-empty string with range") {
+          for {
+            key   <- uuid
+            _     <- set(key, "value", None, None, None)
+            count <- bitCount(key, Some(1 to 3))
+          } yield assert(count)(equalTo(12L))
+        },
+        testM("over non-empty string with range that is too large") {
+          for {
+            key   <- uuid
+            _     <- set(key, "value", None, None, None)
+            count <- bitCount(key, Some(1 to 20))
+          } yield assert(count)(equalTo(16L))
+        },
+        testM("over non-empty string with range that ends with the string") {
+          for {
+            key   <- uuid
+            _     <- set(key, "value", None, None, None)
+            count <- bitCount(key, Some(1 to -1))
+          } yield assert(count)(equalTo(16L))
+        },
+        testM("over non-empty string with range whose start is bigger then end") {
+          for {
+            key   <- uuid
+            _     <- set(key, "value", None, None, None)
+            count <- bitCount(key, Some(3 to 1))
+          } yield assert(count)(equalTo(0L))
+        },
+        testM("over empty string with range") {
+          for {
+            key   <- uuid
+            count <- bitCount(key, Some(1 to 3))
+          } yield assert(count)(equalTo(0L))
+        },
+        testM("over not string with range") {
+          for {
+            key   <- uuid
+            _     <- sAdd(key)("a")
+            count <- bitCount(key, Some(1 to 3)).either
+          } yield assert(count)(isLeft(isSubtype[WrongType](anything)))
+        }
       )
     )
 }
