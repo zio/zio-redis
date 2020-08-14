104a0838ba79b03c3d97d800f6d4cc9978ab23cc
Aleksandar Novakovic <anovakovic01@gmail.com>
2020-08-14 14:07:12 +0200

    Add tests for AggregateInput encoding
    Signed-off-by: Aleksandar Novakovic <anovakovic01@gmail.com>

--- redis/src/test/scala/zio/redis/InputSpec.scala
+++ redis/src/test/scala/zio/redis/InputSpec.scala
@@ -14,6 +14,23 @@ object InputSpec extends BaseSpec {
             result <- Task(AbsTtlInput.encode(AbsTtl))
           } yield assert(result)(equalTo(Chunk.single("$6\r\nABSTTL\r\n")))
         }
+      ),
+      suite("aggregate")(
+        testM("max") {
+          for {
+            result <- Task(AggregateInput.encode(Aggregate.Max))
+          } yield assert(result)(equalTo(Chunk.single("$3\r\nMAX\r\n")))
+        },
+        testM("min") {
+          for {
+            result <- Task(AggregateInput.encode(Aggregate.Min))
+          } yield assert(result)(equalTo(Chunk.single("$3\r\nMIN\r\n")))
+        },
+        testM("sum") {
+          for {
+            result <- Task(AggregateInput.encode(Aggregate.Sum))
+          } yield assert(result)(equalTo(Chunk.single("$3\r\nSUM\r\n")))
+        }
       )
     )
 }
