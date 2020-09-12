6e955498f8171eaf243c4a4a99be37fd7775fdbf
Aleksandar Novakovic <anovakovic01@gmail.com>
2020-09-12 13:50:09 +0200

    Add transactions type to transactions commands
    Signed-off-by: Aleksandar Novakovic <anovakovic01@gmail.com>

--- redis/src/main/scala/zio/redis/api/Transactions.scala
+++ redis/src/main/scala/zio/redis/api/Transactions.scala
@@ -6,9 +6,9 @@ import zio.redis.Output.{ ChunkOutput, UnitOutput }
 import zio.redis.RedisCommand
 
 trait Transactions {
-  final val discard = RedisCommand("DISCARD", NoInput, UnitOutput, Base)
-  final val exec    = RedisCommand("EXEC", NoInput, ChunkOutput, Base)
-  final val multi   = RedisCommand("MULTI", NoInput, UnitOutput, Base)
-  final val unwatch = RedisCommand("UNWATCH", NoInput, UnitOutput, Base)
-  final val watch   = RedisCommand("WATCH", NonEmptyList(StringInput), UnitOutput, Base)
+  final val discard = RedisCommand("DISCARD", NoInput, UnitOutput, Transactions)
+  final val exec    = RedisCommand("EXEC", NoInput, ChunkOutput, Transactions)
+  final val multi   = RedisCommand("MULTI", NoInput, UnitOutput, Transactions)
+  final val unwatch = RedisCommand("UNWATCH", NoInput, UnitOutput, Transactions)
+  final val watch   = RedisCommand("WATCH", NonEmptyList(StringInput), UnitOutput, Transactions)
 }
