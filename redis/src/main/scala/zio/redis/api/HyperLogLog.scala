691f99f08aab94a3e714934a90f1bc16441c06cf
Dejan Mijic <dmijic@acm.org>
2020-10-09 23:21:17 +0200

    Port HLL API

--- redis/src/main/scala/zio/redis/api/HyperLogLog.scala
+++ redis/src/main/scala/zio/redis/api/HyperLogLog.scala
@@ -1,11 +1,24 @@
 package zio.redis.api
 
+import zio.ZIO
 import zio.redis.Input._
 import zio.redis.Output._
-import zio.redis.RedisCommand
+import zio.redis._
 
 trait HyperLogLog {
-  final val pfAdd   = RedisCommand("PFADD", Tuple2(StringInput, NonEmptyList(StringInput)), BoolOutput)
-  final val pfCount = RedisCommand("PFCOUNT", NonEmptyList(StringInput), LongOutput)
-  final val pfMerge = RedisCommand("PFMERGE", Tuple2(StringInput, NonEmptyList(StringInput)), UnitOutput)
+  final val pfAdd =
+    new RedisCommand("PFADD", Tuple2(StringInput, NonEmptyList(StringInput)), BoolOutput) { self =>
+      def apply(a: String, b: String, bs: String*): ZIO[RedisExecutor, RedisError, Boolean] =
+        self.run((a, (b, bs.toList)))
+    }
+
+  final val pfCount = 
+    new RedisCommand("PFCOUNT", NonEmptyList(StringInput), LongOutput) { self =>
+      def apply(a: String, as: String*): ZIO[RedisExecutor, RedisError, Long] = self.run((a, as.toList))
+    }
+
+  final val pfMerge = 
+    new RedisCommand("PFMERGE", Tuple2(StringInput, NonEmptyList(StringInput)), UnitOutput) { self =>
+      def apply(a: String, b: String, bs: String*): ZIO[RedisExecutor, RedisError, Unit] = self.run((a, (b, bs.toList)))
+    }
 }
