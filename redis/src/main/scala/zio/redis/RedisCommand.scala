6762df414205f13b91bb28ba8f8517da9cc49f8d
Dejan Mijic <dmijic@acm.org>
2020-10-09 23:20:45 +0200

    Convert command to abstract class

--- redis/src/main/scala/zio/redis/RedisCommand.scala
+++ redis/src/main/scala/zio/redis/RedisCommand.scala
@@ -2,16 +2,10 @@ package zio.redis
 
 import zio.ZIO
 
-final class RedisCommand[-In, +Out] private (name: String, input: Input[In], output: Output[Out]) {
+abstract class RedisCommand[-In, +Out] private[redis] (name: String, input: Input[In], output: Output[Out]) {
   private[redis] def run(in: In): ZIO[RedisExecutor, RedisError, Out] =
     ZIO
       .accessM[RedisExecutor](_.get.execute(Input.StringInput.encode(name) ++ input.encode(in)))
       .flatMap(out => ZIO.effect(output.unsafeDecode(out)))
       .refineToOrDie[RedisError]
-}
-
-object RedisCommand {
-
-  private[redis] def apply[In, Out](name: String, input: Input[In], output: Output[Out]): RedisCommand[In, Out] =
-    new RedisCommand(name, input, output)
 }
