18590baca9b1b9a361da965eb3abe8a70ef6d9ec
Dejan Mijic <dmijic@acm.org>
2020-10-09 23:20:59 +0200

    Port connection API

--- redis/src/main/scala/zio/redis/api/Connection.scala
+++ redis/src/main/scala/zio/redis/api/Connection.scala
@@ -1,12 +1,28 @@
 package zio.redis.api
 
+import zio.ZIO
 import zio.redis.Input._
 import zio.redis.Output._
-import zio.redis.RedisCommand
+import zio.redis._
 
 trait Connection {
-  final val auth   = RedisCommand("AUTH", StringInput, UnitOutput)
-  final val echo   = RedisCommand("ECHO", StringInput, MultiStringOutput)
-  final val ping   = RedisCommand("PING", Varargs(StringInput), MultiStringOutput)
-  final val select = RedisCommand("SELECT", LongInput, UnitOutput)
+  final val auth =
+    new RedisCommand("AUTH", StringInput, UnitOutput) { self =>
+      def apply(a: String): ZIO[RedisExecutor, RedisError, Unit] = self.run(a)
+    }
+
+  final val echo =
+    new RedisCommand("ECHO", StringInput, MultiStringOutput) { self =>
+      def apply(a: String): ZIO[RedisExecutor, RedisError, String] = self.run(a)
+    }
+
+  final val ping =
+    new RedisCommand("PING", Varargs(StringInput), MultiStringOutput) { self =>
+      def apply(as: String*): ZIO[RedisExecutor, RedisError, String] = self.run(as)
+    }
+
+  final val select =
+    new RedisCommand("SELECT", LongInput, UnitOutput) { self =>
+      def apply(a: Long): ZIO[RedisExecutor, RedisError, Unit] = self.run(a)
+    }
 }
