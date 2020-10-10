e871367053c8be568eb9449cf016c46a4ccbcc4f
Dejan Mijic <dmijic@acm.org>
2020-10-11 00:37:48 +0200

    Port sorted sets API

--- redis/src/main/scala/zio/redis/api/SortedSets.scala
+++ redis/src/main/scala/zio/redis/api/SortedSets.scala
@@ -1,15 +1,27 @@
 package zio.redis.api
 
+import zio.{ Chunk, ZIO }
 import zio.redis.Input._
 import zio.redis.Output._
-import zio.redis.RedisCommand
+import zio.redis._
+import java.time.Duration
+import scala.util.matching.Regex
 
 trait SortedSets {
-  final val bzPopMax = RedisCommand("BZPOPMAX", Tuple2(DurationSecondsInput, NonEmptyList(StringInput)), ChunkOutput)
-  final val bzPopMin = RedisCommand("BZPOPMIN", Tuple2(DurationSecondsInput, NonEmptyList(StringInput)), ChunkOutput)
+  final val bzPopMax =
+    new RedisCommand("BZPOPMAX", Tuple2(DurationSecondsInput, NonEmptyList(StringInput)), ChunkOutput) { self =>
+      def apply(a: Duration, b: String, bs: String*): ZIO[RedisExecutor, RedisError, Chunk[String]] =
+        self.run((a, (b, bs.toList)))
+    }
+
+  final val bzPopMin =
+    new RedisCommand("BZPOPMIN", Tuple2(DurationSecondsInput, NonEmptyList(StringInput)), ChunkOutput) { self =>
+      def apply(a: Duration, b: String, bs: String*): ZIO[RedisExecutor, RedisError, Chunk[String]] =
+        self.run((a, (b, bs.toList)))
+    }
 
   final def zAdd =
-    RedisCommand(
+    new RedisCommand(
       "ZADD",
       Tuple4(
         StringInput,
@@ -18,10 +30,15 @@ trait SortedSets {
         NonEmptyList(MemberScoreInput)
       ),
       LongOutput
-    )
+    ) { self =>
+      def apply(a: String, b: Option[Update] = None, c: Option[Changed] = None)(
+        d: MemberScore,
+        ds: MemberScore*
+      ): ZIO[RedisExecutor, RedisError, Long] = self.run((a, b, c, (d, ds.toList)))
+    }
 
   final def zAddWithIncr =
-    RedisCommand(
+    new RedisCommand(
       "ZADD",
       Tuple5(
         StringInput,
@@ -31,77 +48,183 @@ trait SortedSets {
         NonEmptyList(MemberScoreInput)
       ),
       OptionalOutput(DoubleOutput)
-    )
+    ) { self =>
+      def apply(a: String, b: Option[Update] = None, c: Option[Changed] = None)(
+        d: Increment,
+        e: MemberScore,
+        es: MemberScore*
+      ): ZIO[RedisExecutor, RedisError, Option[Double]] = self.run((a, b, c, d, (e, es.toList)))
+    }
 
-  final val zCard   = RedisCommand("ZCARD", StringInput, LongOutput)
-  final val zCount  = RedisCommand("ZCOUNT", Tuple2(StringInput, RangeInput), LongOutput)
-  final val zIncrBy = RedisCommand("ZINCRBY", Tuple3(StringInput, LongInput, StringInput), DoubleOutput)
+  final val zCard =
+    new RedisCommand("ZCARD", StringInput, LongOutput) { self =>
+      def apply(a: String): ZIO[RedisExecutor, RedisError, Long] = self.run(a)
+    }
 
-  final val zInterStore = RedisCommand(
-    "ZINTERSTORE",
-    Tuple5(
-      StringInput,
-      LongInput,
-      NonEmptyList(StringInput),
-      OptionalInput(AggregateInput),
-      OptionalInput(WeightsInput)
-    ),
-    LongOutput
-  )
+  final val zCount =
+    new RedisCommand("ZCOUNT", Tuple2(StringInput, RangeInput), LongOutput) { self =>
+      def apply(a: String, b: Range): ZIO[RedisExecutor, RedisError, Long] = self.run((a, b))
+    }
 
-  final val zLexCount = RedisCommand("ZLEXCOUNT", Tuple2(StringInput, LexRangeInput), LongOutput)
-  final val zPopMax   = RedisCommand("ZPOPMAX", Tuple2(StringInput, OptionalInput(LongInput)), ChunkOutput)
-  final val zPopMin   = RedisCommand("ZPOPMIN", Tuple2(StringInput, OptionalInput(LongInput)), ChunkOutput)
+  final val zIncrBy =
+    new RedisCommand("ZINCRBY", Tuple3(StringInput, LongInput, StringInput), DoubleOutput) { self =>
+      def apply(a: String, b: Long, c: String): ZIO[RedisExecutor, RedisError, Double] = self.run((a, b, c))
+    }
+
+  final val zInterStore =
+    new RedisCommand(
+      "ZINTERSTORE",
+      Tuple5(
+        StringInput,
+        LongInput,
+        NonEmptyList(StringInput),
+        OptionalInput(AggregateInput),
+        OptionalInput(WeightsInput)
+      ),
+      LongOutput
+    ) { self =>
+      def apply(a: String, b: Long, c: String, cs: String*)(
+        d: Option[Aggregate] = None,
+        e: Option[::[Double]] = None
+      ): ZIO[RedisExecutor, RedisError, Long] = self.run((a, b, (c, cs.toList), d, e))
+
+    }
+
+  final val zLexCount =
+    new RedisCommand("ZLEXCOUNT", Tuple2(StringInput, LexRangeInput), LongOutput) { self =>
+      def apply(a: String, b: LexRange): ZIO[RedisExecutor, RedisError, Long] = self.run((a, b))
+    }
+
+  final val zPopMax =
+    new RedisCommand("ZPOPMAX", Tuple2(StringInput, OptionalInput(LongInput)), ChunkOutput) { self =>
+      def apply(a: String, b: Option[Long] = None): ZIO[RedisExecutor, RedisError, Chunk[String]] = self.run((a, b))
+    }
+
+  final val zPopMin =
+    new RedisCommand("ZPOPMIN", Tuple2(StringInput, OptionalInput(LongInput)), ChunkOutput) { self =>
+      def apply(a: String, b: Option[Long] = None): ZIO[RedisExecutor, RedisError, Chunk[String]] = self.run((a, b))
+    }
 
   final val zRange =
-    RedisCommand("ZRANGE", Tuple3(StringInput, RangeInput, OptionalInput(WithScoresInput)), ChunkOutput)
+    new RedisCommand("ZRANGE", Tuple3(StringInput, RangeInput, OptionalInput(WithScoresInput)), ChunkOutput) { self =>
+      def apply(a: String, b: Range, c: Option[WithScores] = None): ZIO[RedisExecutor, RedisError, Chunk[String]] =
+        self.run((a, b, c))
+    }
 
   final val zRangeByLex =
-    RedisCommand("ZRANGEBYLEX", Tuple3(StringInput, LexRangeInput, OptionalInput(LimitInput)), ChunkOutput)
+    new RedisCommand("ZRANGEBYLEX", Tuple3(StringInput, LexRangeInput, OptionalInput(LimitInput)), ChunkOutput) {
+      self =>
+      def apply(a: String, b: LexRange, c: Option[Limit] = None): ZIO[RedisExecutor, RedisError, Chunk[String]] =
+        self.run((a, b, c))
+    }
 
-  final val zRangeByScore = RedisCommand(
-    "ZRANGEBYSCORE",
-    Tuple4(StringInput, ScoreRangeInput, OptionalInput(WithScoresInput), OptionalInput(LimitInput)),
-    ChunkOutput
-  )
+  final val zRangeByScore =
+    new RedisCommand(
+      "ZRANGEBYSCORE",
+      Tuple4(StringInput, ScoreRangeInput, OptionalInput(WithScoresInput), OptionalInput(LimitInput)),
+      ChunkOutput
+    ) { self =>
+      def apply(
+        a: String,
+        b: ScoreRange,
+        c: Option[WithScores] = None,
+        d: Option[Limit] = None
+      ): ZIO[RedisExecutor, RedisError, Chunk[String]] = self.run((a, b, c, d))
+    }
 
-  final val zRank            = RedisCommand("ZRANK", Tuple2(StringInput, StringInput), OptionalOutput(LongOutput))
-  final val zRem             = RedisCommand("ZREM", Tuple2(StringInput, NonEmptyList(StringInput)), LongOutput)
-  final val zRemRangeByLex   = RedisCommand("ZREMRANGEBYLEX", Tuple2(StringInput, LexRangeInput), LongOutput)
-  final val zRemRangeByRank  = RedisCommand("ZREMRANGEBYRANK", Tuple2(StringInput, RangeInput), LongOutput)
-  final val zRemRangeByScore = RedisCommand("ZREMRANGEBYSCORE", Tuple2(StringInput, ScoreRangeInput), LongOutput)
+  final val zRank =
+    new RedisCommand("ZRANK", Tuple2(StringInput, StringInput), OptionalOutput(LongOutput)) { self =>
+      def apply(a: String, b: String): ZIO[RedisExecutor, RedisError, Option[Long]] = self.run((a, b))
+    }
+
+  final val zRem =
+    new RedisCommand("ZREM", Tuple2(StringInput, NonEmptyList(StringInput)), LongOutput) { self =>
+      def apply(a: String, b: String, bs: String*): ZIO[RedisExecutor, RedisError, Long] = self.run((a, (b, bs.toList)))
+    }
+
+  final val zRemRangeByLex =
+    new RedisCommand("ZREMRANGEBYLEX", Tuple2(StringInput, LexRangeInput), LongOutput) { self =>
+      def apply(a: String, b: LexRange): ZIO[RedisExecutor, RedisError, Long] = self.run((a, b))
+    }
+
+  final val zRemRangeByRank =
+    new RedisCommand("ZREMRANGEBYRANK", Tuple2(StringInput, RangeInput), LongOutput) { self =>
+      def apply(a: String, b: Range): ZIO[RedisExecutor, RedisError, Long] = self.run((a, b))
+    }
+
+  final val zRemRangeByScore =
+    new RedisCommand("ZREMRANGEBYSCORE", Tuple2(StringInput, ScoreRangeInput), LongOutput) { self =>
+      def apply(a: String, b: ScoreRange): ZIO[RedisExecutor, RedisError, Long] = self.run((a, b))
+    }
 
   final val zRevRange =
-    RedisCommand("ZREVRANGE", Tuple3(StringInput, RangeInput, OptionalInput(WithScoresInput)), ChunkOutput)
+    new RedisCommand("ZREVRANGE", Tuple3(StringInput, RangeInput, OptionalInput(WithScoresInput)), ChunkOutput) {
+      self =>
+      def apply(a: String, b: Range, c: Option[WithScores] = None): ZIO[RedisExecutor, RedisError, Chunk[String]] =
+        self.run((a, b, c))
+    }
 
   final val zRevRangeByLex =
-    RedisCommand("ZREVRANGEBYLEX", Tuple3(StringInput, LexRangeInput, OptionalInput(LimitInput)), ChunkOutput)
+    new RedisCommand("ZREVRANGEBYLEX", Tuple3(StringInput, LexRangeInput, OptionalInput(LimitInput)), ChunkOutput) {
+      self =>
+      def apply(a: String, b: LexRange, c: Option[Limit] = None): ZIO[RedisExecutor, RedisError, Chunk[String]] =
+        self.run((a, b, c))
+    }
 
-  final val zRevRangeByScore = RedisCommand(
-    "ZREVRANGEBYSCORE",
-    Tuple4(StringInput, ScoreRangeInput, OptionalInput(WithScoresInput), OptionalInput(LimitInput)),
-    ChunkOutput
-  )
+  final val zRevRangeByScore =
+    new RedisCommand(
+      "ZREVRANGEBYSCORE",
+      Tuple4(StringInput, ScoreRangeInput, OptionalInput(WithScoresInput), OptionalInput(LimitInput)),
+      ChunkOutput
+    ) { self =>
+      def apply(
+        a: String,
+        b: ScoreRange,
+        c: Option[WithScores] = None,
+        d: Option[Limit] = None
+      ): ZIO[RedisExecutor, RedisError, Chunk[String]] =
+        self.run((a, b, c, d))
+    }
 
-  final val zRevRank = RedisCommand("ZREVRANK", Tuple2(StringInput, StringInput), OptionalOutput(LongOutput))
+  final val zRevRank =
+    new RedisCommand("ZREVRANK", Tuple2(StringInput, StringInput), OptionalOutput(LongOutput)) { self =>
+      def apply(a: String, b: String): ZIO[RedisExecutor, RedisError, Option[Long]] = self.run((a, b))
+    }
 
-  final val zScan = RedisCommand(
-    "ZSCAN",
-    Tuple4(StringInput, LongInput, OptionalInput(RegexInput), OptionalInput(CountInput)),
-    ScanOutput
-  )
+  final val zScan =
+    new RedisCommand(
+      "ZSCAN",
+      Tuple4(StringInput, LongInput, OptionalInput(RegexInput), OptionalInput(CountInput)),
+      ScanOutput
+    ) { self =>
+      def apply(
+        a: String,
+        b: Long,
+        c: Option[Regex] = None,
+        d: Option[Count] = None
+      ): ZIO[RedisExecutor, RedisError, (String, Chunk[String])] = self.run((a, b, c, d))
+    }
 
-  final val zScore = RedisCommand("ZSCORE", Tuple2(StringInput, StringInput), OptionalOutput(DoubleOutput))
+  final val zScore =
+    new RedisCommand("ZSCORE", Tuple2(StringInput, StringInput), OptionalOutput(DoubleOutput)) { self =>
+      def apply(a: String, b: String): ZIO[RedisExecutor, RedisError, Option[Double]] = self.run((a, b))
+    }
 
-  final val zUnionStore = RedisCommand(
-    "ZUNIONSTORE",
-    Tuple5(
-      StringInput,
-      LongInput,
-      NonEmptyList(StringInput),
-      OptionalInput(WeightsInput),
-      OptionalInput(AggregateInput)
-    ),
-    LongOutput
-  )
+  final val zUnionStore =
+    new RedisCommand(
+      "ZUNIONSTORE",
+      Tuple5(
+        StringInput,
+        LongInput,
+        NonEmptyList(StringInput),
+        OptionalInput(WeightsInput),
+        OptionalInput(AggregateInput)
+      ),
+      LongOutput
+    ) { self =>
+      def apply(a: String, b: Long, c: String, cs: String*)(
+        d: Option[::[Double]] = None,
+        e: Option[Aggregate] = None
+      ): ZIO[RedisExecutor, RedisError, Long] = self.run((a, b, (c, cs.toList), d, e))
+    }
 }
