4b4fcc7225ea18f46e0b1d2446dbd497baa45e1f
Dejan Mijic <dmijic@acm.org>
2020-10-11 02:55:16 +0200

    Fix reflective access in lists api

--- redis/src/main/scala/zio/redis/api/Lists.scala
+++ redis/src/main/scala/zio/redis/api/Lists.scala
@@ -7,95 +7,78 @@ import zio.redis._
 import java.time.Duration
 
 trait Lists {
-  final val brPopLPush =
+  import Lists._
+
+  final def brPopLPush(a: String, b: String, c: Duration): ZIO[RedisExecutor, RedisError, Option[String]] =
+    BrPopLPush.run((a, b, c))
+
+  final def lIndex(a: String, b: Long): ZIO[RedisExecutor, RedisError, Option[String]] = LIndex.run((a, b))
+
+  final def lLen(a: String): ZIO[RedisExecutor, RedisError, Long] = LLen.run(a)
+
+  final def lPop(a: String): ZIO[RedisExecutor, RedisError, Option[String]] = LPop.run(a)
+
+  final def lPush(a: String, b: String, bs: String*): ZIO[RedisExecutor, RedisError, Long] =
+    LPush.run((a, (b, bs.toList)))
+
+  final def lPushX(a: String, b: String, bs: String*): ZIO[RedisExecutor, RedisError, Long] =
+    LPushX.run((a, (b, bs.toList)))
+
+  final def lRange(a: String, b: Range): ZIO[RedisExecutor, RedisError, Chunk[String]] = LRange.run((a, b))
+
+  final def lRem(a: String, b: Long, c: String): ZIO[RedisExecutor, RedisError, Long] = LRem.run((a, b, c))
+
+  final def lSet(a: String, b: Long, c: String): ZIO[RedisExecutor, RedisError, Unit] = LSet.run((a, b, c))
+
+  final def lTrim(a: String, b: Range): ZIO[RedisExecutor, RedisError, Unit] = LTrim.run((a, b))
+
+  final def rPop(a: String): ZIO[RedisExecutor, RedisError, Option[String]] = RPop.run(a)
+
+  final def rPopLPush(a: String, b: String): ZIO[RedisExecutor, RedisError, Option[String]] = RPopLPush.run((a, b))
+
+  final def rPush(a: String, b: String, bs: String*): ZIO[RedisExecutor, RedisError, Long] =
+    RPush.run((a, (b, bs.toList)))
+
+  final def rPushX(a: String, b: String, bs: String*): ZIO[RedisExecutor, RedisError, Long] =
+    RPushX.run((a, (b, bs.toList)))
+
+  final def blPop(a: String, as: String*)(b: Duration): ZIO[RedisExecutor, RedisError, Option[(String, String)]] =
+    BlPop.run(((a, as.toList), b))
+
+  final def brPop(a: String, as: String*)(b: Duration): ZIO[RedisExecutor, RedisError, Option[(String, String)]] =
+    BrPop.run(((a, as.toList), b))
+
+  final def lInsert(a: String, b: Position, c: String, d: String): ZIO[RedisExecutor, RedisError, Long] =
+    LInsert.run((a, b, c, d))
+}
+
+private[api] object Lists {
+  final val BrPopLPush =
     new RedisCommand(
       "BRPOPLPUSH",
       Tuple3(StringInput, StringInput, DurationSecondsInput),
       OptionalOutput(MultiStringOutput)
-    ) { self =>
-      def apply(a: String, b: String, c: Duration): ZIO[RedisExecutor, RedisError, Option[String]] = self.run((a, b, c))
-    }
+    )
 
-  final val lIndex =
-    new RedisCommand("LINDEX", Tuple2(StringInput, LongInput), OptionalOutput(MultiStringOutput)) { self =>
-      def apply(a: String, b: Long): ZIO[RedisExecutor, RedisError, Option[String]] = self.run((a, b))
-    }
+  final val LIndex = new RedisCommand("LINDEX", Tuple2(StringInput, LongInput), OptionalOutput(MultiStringOutput))
+  final val LLen   = new RedisCommand("LLEN", StringInput, LongOutput)
+  final val LPop   = new RedisCommand("LPOP", StringInput, OptionalOutput(MultiStringOutput))
+  final val LPush  = new RedisCommand("LPUSH", Tuple2(StringInput, NonEmptyList(StringInput)), LongOutput)
+  final val LPushX = new RedisCommand("LPUSHX", Tuple2(StringInput, NonEmptyList(StringInput)), LongOutput)
+  final val LRange = new RedisCommand("LRANGE", Tuple2(StringInput, RangeInput), ChunkOutput)
+  final val LRem   = new RedisCommand("LREM", Tuple3(StringInput, LongInput, StringInput), LongOutput)
+  final val LSet   = new RedisCommand("LSET", Tuple3(StringInput, LongInput, StringInput), UnitOutput)
+  final val LTrim  = new RedisCommand("LTRIM", Tuple2(StringInput, RangeInput), UnitOutput)
+  final val RPop   = new RedisCommand("RPOP", StringInput, OptionalOutput(MultiStringOutput))
 
-  final val lLen =
-    new RedisCommand("LLEN", StringInput, LongOutput) { self =>
-      def apply(a: String): ZIO[RedisExecutor, RedisError, Long] = self.run(a)
-    }
+  final val RPopLPush =
+    new RedisCommand("RPOPLPUSH", Tuple2(StringInput, StringInput), OptionalOutput(MultiStringOutput))
 
-  final val lPop =
-    new RedisCommand("LPOP", StringInput, OptionalOutput(MultiStringOutput)) { self =>
-      def apply(a: String): ZIO[RedisExecutor, RedisError, Option[String]] = self.run(a)
-    }
+  final val RPush  = new RedisCommand("RPUSH", Tuple2(StringInput, NonEmptyList(StringInput)), LongOutput)
+  final val RPushX = new RedisCommand("RPUSHX", Tuple2(StringInput, NonEmptyList(StringInput)), LongOutput)
+  final val BlPop  = new RedisCommand("BLPOP", Tuple2(NonEmptyList(StringInput), DurationSecondsInput), KeyElemOutput)
+  final val BrPop  = new RedisCommand("BRPOP", Tuple2(NonEmptyList(StringInput), DurationSecondsInput), KeyElemOutput)
 
-  final val lPush =
-    new RedisCommand("LPUSH", Tuple2(StringInput, NonEmptyList(StringInput)), LongOutput) { self =>
-      def apply(a: String, b: String, bs: String*): ZIO[RedisExecutor, RedisError, Long] = self.run((a, (b, bs.toList)))
-    }
-
-  final val lPushX =
-    new RedisCommand("LPUSHX", Tuple2(StringInput, NonEmptyList(StringInput)), LongOutput) { self =>
-      def apply(a: String, b: String, bs: String*): ZIO[RedisExecutor, RedisError, Long] = self.run((a, (b, bs.toList)))
-    }
-
-  final val lRange =
-    new RedisCommand("LRANGE", Tuple2(StringInput, RangeInput), ChunkOutput) { self =>
-      def apply(a: String, b: Range): ZIO[RedisExecutor, RedisError, Chunk[String]] = self.run((a, b))
-    }
-
-  final val lRem =
-    new RedisCommand("LREM", Tuple3(StringInput, LongInput, StringInput), LongOutput) { self =>
-      def apply(a: String, b: Long, c: String): ZIO[RedisExecutor, RedisError, Long] = self.run((a, b, c))
-    }
-
-  final val lSet =
-    new RedisCommand("LSET", Tuple3(StringInput, LongInput, StringInput), UnitOutput) { self =>
-      def apply(a: String, b: Long, c: String): ZIO[RedisExecutor, RedisError, Unit] = self.run((a, b, c))
-    }
-
-  final val lTrim =
-    new RedisCommand("LTRIM", Tuple2(StringInput, RangeInput), UnitOutput) { self =>
-      def apply(a: String, b: Range): ZIO[RedisExecutor, RedisError, Unit] = self.run((a, b))
-    }
-
-  final val rPop =
-    new RedisCommand("RPOP", StringInput, OptionalOutput(MultiStringOutput)) { self =>
-      def apply(a: String): ZIO[RedisExecutor, RedisError, Option[String]] = self.run(a)
-    }
-
-  final val rPopLPush =
-    new RedisCommand("RPOPLPUSH", Tuple2(StringInput, StringInput), OptionalOutput(MultiStringOutput)) { self =>
-      def apply(a: String, b: String): ZIO[RedisExecutor, RedisError, Option[String]] = self.run((a, b))
-    }
-
-  final val rPush =
-    new RedisCommand("RPUSH", Tuple2(StringInput, NonEmptyList(StringInput)), LongOutput) { self =>
-      def apply(a: String, b: String, bs: String*): ZIO[RedisExecutor, RedisError, Long] = self.run((a, (b, bs.toList)))
-    }
-
-  final val rPushX =
-    new RedisCommand("RPUSHX", Tuple2(StringInput, NonEmptyList(StringInput)), LongOutput) { self =>
-      def apply(a: String, b: String, bs: String*): ZIO[RedisExecutor, RedisError, Long] = self.run((a, (b, bs.toList)))
-    }
-
-  final val blPop =
-    new RedisCommand("BLPOP", Tuple2(NonEmptyList(StringInput), DurationSecondsInput), KeyElemOutput) { self =>
-      def apply(a: String, as: String*)(b: Duration): ZIO[RedisExecutor, RedisError, Option[(String, String)]] =
-        self.run(((a, as.toList), b))
-    }
-
-  final val brPop =
-    new RedisCommand("BRPOP", Tuple2(NonEmptyList(StringInput), DurationSecondsInput), KeyElemOutput) { self =>
-      def apply(a: String, as: String*)(b: Duration): ZIO[RedisExecutor, RedisError, Option[(String, String)]] =
-        self.run(((a, as.toList), b))
-    }
-
-  final val lInsert =
-    new RedisCommand("LINSERT", Tuple4(StringInput, PositionInput, StringInput, StringInput), LongOutput) { self =>
-      def apply(a: String, b: Position, c: String, d: String): ZIO[RedisExecutor, RedisError, Long] =
-        self.run((a, b, c, d))
-    }
+  final val LInsert =
+    new RedisCommand("LINSERT", Tuple4(StringInput, PositionInput, StringInput, StringInput), LongOutput)
 }
