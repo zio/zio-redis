ba299a419e6b0385f01249fb4089f6508f1c26fe
Dejan Mijic <dmijic@acm.org>
2020-10-11 02:46:42 +0200

    Make command class

--- redis/src/main/scala/zio/redis/RedisCommand.scala
+++ redis/src/main/scala/zio/redis/RedisCommand.scala
@@ -2,7 +2,7 @@ package zio.redis
 
 import zio.ZIO
 
-abstract class RedisCommand[-In, +Out] private[redis] (name: String, input: Input[In], output: Output[Out]) {
+final class RedisCommand[-In, +Out] private[redis] (name: String, input: Input[In], output: Output[Out]) {
   private[redis] def run(in: In): ZIO[RedisExecutor, RedisError, Out] =
     ZIO
       .accessM[RedisExecutor](_.get.execute(Input.StringInput.encode(name) ++ input.encode(in)))
