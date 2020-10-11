42a4f738ae3ea57e130fcbd79bbad0b8f12865b8
Dejan Mijic <dmijic@acm.org>
2020-10-11 02:46:49 +0200

    Fix reflective access in connections api

--- redis/src/main/scala/zio/redis/api/Connection.scala
+++ redis/src/main/scala/zio/redis/api/Connection.scala
@@ -6,23 +6,20 @@ import zio.redis.Output._
 import zio.redis._
 
 trait Connection {
-  final val auth =
-    new RedisCommand("AUTH", StringInput, UnitOutput) { self =>
-      def apply(a: String): ZIO[RedisExecutor, RedisError, Unit] = self.run(a)
-    }
+  import Connection._
 
-  final val echo =
-    new RedisCommand("ECHO", StringInput, MultiStringOutput) { self =>
-      def apply(a: String): ZIO[RedisExecutor, RedisError, String] = self.run(a)
-    }
+  final def auth(a: String): ZIO[RedisExecutor, RedisError, Unit] = Auth.run(a)
 
-  final val ping =
-    new RedisCommand("PING", Varargs(StringInput), MultiStringOutput) { self =>
-      def apply(as: String*): ZIO[RedisExecutor, RedisError, String] = self.run(as)
-    }
+  final def echo(a: String): ZIO[RedisExecutor, RedisError, String] = Echo.run(a)
 
-  final val select =
-    new RedisCommand("SELECT", LongInput, UnitOutput) { self =>
-      def apply(a: Long): ZIO[RedisExecutor, RedisError, Unit] = self.run(a)
-    }
+  final def ping(as: String*): ZIO[RedisExecutor, RedisError, String] = Ping.run(as)
+
+  final def select(a: Long): ZIO[RedisExecutor, RedisError, Unit] = Select.run(a)
+}
+
+private[api] object Connection {
+  final val Auth   = new RedisCommand("AUTH", StringInput, UnitOutput)
+  final val Echo   = new RedisCommand("ECHO", StringInput, MultiStringOutput)
+  final val Ping   = new RedisCommand("PING", Varargs(StringInput), MultiStringOutput)
+  final val Select = new RedisCommand("SELECT", LongInput, UnitOutput)
 }
