f47dbbd01187e8a98b3b64a795da86a04e7ba79b
Justin Heyes-Jones <justinhj@gmail.com>
2020-11-21 09:55:34 -0800

    Update formatting

--- redis/src/main/scala/zio/redis/api/Sets.scala
+++ redis/src/main/scala/zio/redis/api/Sets.scala
@@ -10,9 +10,10 @@ import zio.{ Chunk, ZIO }
 trait Sets {
   import Sets._
 
-  /** Add one or more members to a set
+  /**
+   * Add one or more members to a set
    *
-    * @param key Key of set to add to
+   * @param key Key of set to add to
    * @param member first member to add
    * @param members subsequent members to add
    * @return Returns the number of elements that were added to the set, not including all the elements already present into the set
@@ -20,25 +21,28 @@ trait Sets {
   final def sAdd(key: String, member: String, members: String*): ZIO[RedisExecutor, RedisError, Long] =
     SAdd.run((key, (member, members.toList)))
 
-  /** Get the number of members in a set
+  /**
+   * Get the number of members in a set
    *
-    * @param key Key of set to get the number of members of
+   * @param key Key of set to get the number of members of
    * @return Returns the cardinality (number of elements) of the set, or 0 if key does not exist
    */
   final def sCard(key: String): ZIO[RedisExecutor, RedisError, Long] = SCard.run(key)
 
-  /** Subtract multiple sets
+  /**
+   * Subtract multiple sets
    *
-    * @param key Key of the set to subtract from
+   * @param key Key of the set to subtract from
    * @param keys Keys of the sets to subtract
    * @return Returns the members of the set resulting from the difference between the first set and all the successive sets
    */
   final def sDiff(key: String, keys: String*): ZIO[RedisExecutor, RedisError, Chunk[String]] =
     SDiff.run((key, keys.toList))
 
-  /** Subtract multiple sets and store the resulting set in a key
+  /**
+   * Subtract multiple sets and store the resulting set in a key
    *
-    * @param destination Key of set to store the resulting set
+   * @param destination Key of set to store the resulting set
    * @param key Key of set to be subtracted from
    * @param keys Keys of sets to subtract
    * @return Returns the number of elements in the resulting set
@@ -46,18 +50,20 @@ trait Sets {
   final def sDiffStore(destination: String, key: String, keys: String*): ZIO[RedisExecutor, RedisError, Long] =
     SDiffStore.run((destination, (key, keys.toList)))
 
-  /** Intersect multiple sets and store the resulting set in a key
+  /**
+   * Intersect multiple sets and store the resulting set in a key
    *
-    * @param destination Key of set to store the resulting set
+   * @param destination Key of set to store the resulting set
    * @param keys Keys of the sets to intersect with each other
    * @return Returns the members of the set resulting from the intersection of all the given sets
    */
   final def sInter(destination: String, keys: String*): ZIO[RedisExecutor, RedisError, Chunk[String]] =
     SInter.run((destination, keys.toList))
 
-  /** Intersect multiple sets and store the resulting set in a key
+  /**
+   * Intersect multiple sets and store the resulting set in a key
    *
-    * @param destination Key of set to store the resulting set
+   * @param destination Key of set to store the resulting set
    * @param key Key of first set to intersect
    * @param keys Keys of subsequent sets to intersect
    * @return Returns the number of elements in the resulting set
@@ -69,25 +75,28 @@ trait Sets {
   ): ZIO[RedisExecutor, RedisError, Long] =
     SInterStore.run((destination, (key, keys.toList)))
 
-  /** Determine if a given value is a member of a set
+  /**
+   * Determine if a given value is a member of a set
    *
-    * @param key
+   * @param key
    * @param member
    * @return Returns 1 if the element is a member of the set. 0 if the element is not a member of the set, or if key does not exist
    */
   final def sIsMember(key: String, member: String): ZIO[RedisExecutor, RedisError, Boolean] =
     SIsMember.run((key, member))
 
-  /** Get all the members in a set
+  /**
+   * Get all the members in a set
    *
-    * @param key Key of the set to get the members of
+   * @param key Key of the set to get the members of
    * @return Returns the members of the set
    */
   final def sMembers(key: String): ZIO[RedisExecutor, RedisError, Chunk[String]] = SMembers.run(key)
 
-  /** Move a member from one set to another
+  /**
+   * Move a member from one set to another
    *
-    * @param source Key of the set to move the member from
+   * @param source Key of the set to move the member from
    * @param destination Key of the set to move the member to
    * @param member Element to move
    * @return Returns 1 if the element was moved. 0 if it was not found.
@@ -95,27 +104,30 @@ trait Sets {
   final def sMove(source: String, destination: String, member: String): ZIO[RedisExecutor, RedisError, Boolean] =
     SMove.run((source, destination, member))
 
-  /** Remove and return one or multiple random members from a set
+  /**
+   * Remove and return one or multiple random members from a set
    *
-    * @param key Key of the set to remove items from
+   * @param key Key of the set to remove items from
    * @param count Number of elements to remove
    * @return Returns the elements removed
    */
   final def sPop(key: String, count: Option[Long] = None): ZIO[RedisExecutor, RedisError, Chunk[String]] =
     SPop.run((key, count))
 
-  /** Get one or multiple random members from a set
+  /**
+   * Get one or multiple random members from a set
    *
-    * @param key Key of the set to get members from
+   * @param key Key of the set to get members from
    * @param count Number of elements to randomly get
    * @return Returns the random members
    */
   final def sRandMember(key: String, count: Option[Long] = None): ZIO[RedisExecutor, RedisError, Chunk[String]] =
     SRandMember.run((key, count))
 
-  /** Remove one of more members from a set
+  /**
+   * Remove one of more members from a set
    *
-    * @param key Key of the set to remove members from
+   * @param key Key of the set to remove members from
    * @param member Value of the first element to remove
    * @param members Subsequent values of elements to remove
    * @return Returns the number of members that were removed from the set, not including non existing members
@@ -123,12 +135,13 @@ trait Sets {
   final def sRem(key: String, member: String, members: String*): ZIO[RedisExecutor, RedisError, Long] =
     SRem.run((key, (member, members.toList)))
 
-  /** Incrementally iterate Set elements
+  /**
+   * Incrementally iterate Set elements
    *
-    * This is a cursor based scan of an entire set. Call initially with cursor set to 0 and on subsequent
+   * This is a cursor based scan of an entire set. Call initially with cursor set to 0 and on subsequent
    * calls pass the return value as the next cursor.
    *
-    * @param key Key of the set to scan
+   * @param key Key of the set to scan
    * @param cursor Cursor to use for this iteration of scan
    * @param regex Glob-style pattern that filters which elements are returned
    * @param count Count of elements. Roughly this number will be returned by Redis if possible
@@ -144,16 +157,17 @@ trait Sets {
   /**
    * Add multiple sets
    *
-    * @param key Key of the first set to add
+   * @param key Key of the first set to add
    * @param keys Keys of the subsequent sets to add
    * @return Returns a list with members of the resulting set
    */
   final def sUnion(key: String, keys: String*): ZIO[RedisExecutor, RedisError, Chunk[String]] =
     SUnion.run((key, keys.toList))
 
-  /** Add multiple sets and add the resulting set in a key
+  /**
+   * Add multiple sets and add the resulting set in a key
    *
-    * @param destination Key of destination to store the result
+   * @param destination Key of destination to store the result
    * @param key Key of first set to add
    * @param keys Subsequent keys of sets to add
    * @return Returns the number of elements in the resulting set
