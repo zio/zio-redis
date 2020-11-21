31a8d7deb36ca49486073bf15c586307a7f1ac30
Michal Lacko <viso.laci@gmail.com>
2020-11-21 17:04:28 +0100

    issue/160 WIP format test

--- redis/src/test/scala/zio/redis/PubSubSpec.scala
+++ redis/src/test/scala/zio/redis/PubSubSpec.scala
@@ -6,16 +6,14 @@ import zio.test._
 trait PubSubSpec extends BaseSpec {
 
   val pubSubSuite =
-    suite("pubsub")(
-      suite("publish subscribe")(
-        testM("publish one message and receive it as subscriber") {
-          val channel = "channel_1"
-          val msg = "msg_1"
-          val subStream = subscribe(channel)
+    suite("pubsub")(suite("publish subscribe")(testM("publish one message and receive it as subscriber") {
+      val channel   = "channel_1"
+      val msg       = "msg_1"
+      val subStream = subscribe(channel)
 
-          for {
-            _      <- publish(channel, msg)
-            msgGot <- subStream.runHead
-          } yield assert(msgGot)(isSome(equalTo(msg)))
-        }))
+      for {
+        _      <- publish(channel, msg)
+        msgGot <- subStream.runHead
+      } yield assert(msgGot)(isSome(equalTo(msg)))
+    }))
 }
