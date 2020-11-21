6ad47f02070f443a4da5253d9bb5899b0973b763
Dejan Mijić <dmijic@acm.org>
2020-11-21 17:49:19 +0100

    Simplify ByteStream (#205)

--- redis/src/test/scala/zio/redis/ByteStreamSpec.scala
+++ redis/src/test/scala/zio/redis/ByteStreamSpec.scala
@@ -2,23 +2,21 @@ package zio.redis
 
 import java.nio.charset.StandardCharsets
 
-import zio.Chunk
 import zio.logging.Logging
 import zio.test.Assertion._
 import zio.test._
+import zio.{ Chunk, ZIO }
 
 object ByteStreamSpec extends BaseSpec {
   override def spec: ZSpec[Environment, Failure] =
     suite("Byte stream")(
       testM("can write and read") {
-        ByteStream.connect.use { rw =>
-          rw.write(
-            Chunk.fromArray("*2\r\n$7\r\nCOMMAND\r\n$4\r\nINFO\r\n$3\r\nGET\r\n".getBytes(StandardCharsets.UTF_8))
-          ) *>
-            rw.read.runHead
-              .map(result => assert(result)(isSome(equalTo('*'.toByte))))
-
-        }
+        for {
+          stream <- ZIO.service[ByteStream.Service]
+          data    = Chunk.fromArray("*2\r\n$7\r\nCOMMAND\r\n$4\r\nINFO\r\n$3\r\nGET\r\n".getBytes(StandardCharsets.UTF_8))
+          _      <- stream.write(data)
+          res    <- stream.read.runHead
+        } yield assert(res)(isSome(equalTo('*'.toByte)))
       }
-    ).provideCustomLayerShared(Logging.ignore >>> ByteStream.socketLoopback().orDie)
+    ).provideCustomLayer(Logging.ignore >>> ByteStream.loopback().orDie)
 }
