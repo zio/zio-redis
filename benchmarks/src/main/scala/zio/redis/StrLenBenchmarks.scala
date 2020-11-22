c90174813c74a4a1122faa668b71707f3943efe2
Kostyantyn Spitsyn <konstantin.spitsyn@gmail.com>
2020-11-22 09:53:27 +0100

    add one more benchmark

---
+++ benchmarks/src/main/scala/zio/redis/StrLenBenchmarks.scala
@@ -0,0 +1,55 @@
+package zio.redis
+
+import java.util.concurrent.TimeUnit
+
+import org.openjdk.jmh.annotations._
+import zio.ZIO
+
+@State(Scope.Thread)
+@BenchmarkMode(Array(Mode.Throughput, Mode.AverageTime))
+@OutputTimeUnit(TimeUnit.SECONDS)
+@Measurement(iterations = 15)
+@Warmup(iterations = 15)
+@Fork(2)
+class StrLenBenchmarks {
+  import BenchmarkRuntime._
+  import BenchmarksUtils._
+  import RedisClients._
+
+  @Param(Array("500"))
+  private var count: Int = _
+
+  private var items: List[String] = _
+
+  @Setup(Level.Trial)
+  def setup(): Unit = {
+    items = (0 to count).toList.map(_.toString)
+    zioUnsafeRun(ZIO.foreach_(items)(i => set(i, i)))
+  }
+
+  @Benchmark
+  def laserdisc(): Unit = {
+    import _root_.laserdisc.fs2._
+    import _root_.laserdisc.{all => cmd, _}
+    import cats.instances.list._
+    import cats.syntax.foldable._
+    unsafeClientRun[LaserDiskClient](c => items.traverse_(i => c.send(cmd.strlen(Key.unsafeFrom(i)))))
+  }
+
+  @Benchmark
+  def rediculous(): Unit = {
+    import cats.implicits._
+    import io.chrisdavenport.rediculous._
+    unsafeClientRun[RediculousClient](c => items.traverse_(i => RedisCommands.strlen[RedisIO](i).run(c)))
+  }
+
+  @Benchmark
+  def redis4cats(): Unit = {
+    import cats.instances.list._
+    import cats.syntax.foldable._
+    unsafeClientRun[Redis4CatsClient[String]](c => items.traverse_(i => c.strLen(i)))
+  }
+
+  @Benchmark
+  def zio(): Unit = zioUnsafeRun(ZIO.foreach_(items)(strLen))
+}
