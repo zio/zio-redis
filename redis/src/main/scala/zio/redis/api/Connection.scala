e19e9d94c66bf817fb48deaef0c70353393039a3
Ilias Antoniou <helias1990@hotmail.com>
2020-11-22 16:03:30 +0000

    Document connection API

--- redis/src/main/scala/zio/redis/api/Connection.scala
+++ redis/src/main/scala/zio/redis/api/Connection.scala
@@ -8,13 +8,45 @@ import zio.redis._
 trait Connection {
   import Connection._
 
-  final def auth(a: String): ZIO[RedisExecutor, RedisError, Unit] = Auth.run(a)
+  /**
+   * Authenticates the current connection to the server in two cases:
+   *  - If the Redis server is password protected via the the ''requirepass'' option
+   *  - If a Redis 6.0 instance, or greater, is using the [[https://redis.io/topics/acl Redis ACL system]]. In this case
+   *  it is assumed that the implicit username is ''default''
+   *
+   * @param password the password used to authenticate the connection
+   * @return if the password provided via AUTH matches the password in the configuration file, the Unit value is
+   *         returned and the server starts accepting commands.
+   *         Otherwise, an error is returned and the client needs to try a new password
+   */
+  final def auth(password: String): ZIO[RedisExecutor, RedisError, Unit] = Auth.run(password)
 
-  final def echo(a: String): ZIO[RedisExecutor, RedisError, String] = Echo.run(a)
+  /**
+   * Echoes the given string
+   *
+   * @param message the message to be echoed
+   * @return the message
+   */
+  final def echo(message: String): ZIO[RedisExecutor, RedisError, String] = Echo.run(message)
 
-  final def ping(a: Option[String] = None): ZIO[RedisExecutor, RedisError, String] = Ping.run(a)
+  /**
+   * Pings the server
+   *
+   * @param message the optional message to receive back from server
+   * @return PONG if no argument is provided, otherwise return a copy of the argument as a bulk.
+   *         This command is often used to test if a connection is still alive, or to measure latency
+   */
+  final def ping(message: Option[String] = None): ZIO[RedisExecutor, RedisError, String] = Ping.run(message)
 
-  final def select(a: Long): ZIO[RedisExecutor, RedisError, Unit] = Select.run(a)
+  /**
+   * Changes the database for the current connection to the database having the specified numeric index.
+   * The currently selected database is a property of the connection; clients should track the selected database and
+   * re-select it on reconnection
+   *
+   * @param index the database index. The index is zero-based. New connections always use the database 0
+   * @return the Unit value
+   */
+  final def select(index: Long): ZIO[RedisExecutor, RedisError, Unit] = Select.run(index)
 }
 
 private[redis] object Connection {
