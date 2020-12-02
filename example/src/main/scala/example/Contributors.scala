c87730a4eee0925ea7abe77872c26b0d46a725ca
Dejan Mijic <dmijic@acm.org>
2020-12-02 02:51:49 +0100

    Inline decode

--- example/src/main/scala/example/Contributors.scala
+++ example/src/main/scala/example/Contributors.scala
@@ -2,7 +2,6 @@ package example
 
 import example.Contributor._
 import example.ApiError._
-import io.circe.Error
 import io.circe.parser.decode
 import io.circe.syntax._
 import sttp.client.asynchttpclient.zio.SttpClient
@@ -27,26 +26,29 @@ object Contributors {
           sMembers(repository).flatMap { response => // key should be unique combination of org + repo
             if (response.isEmpty)
               for {
-                contributors <- getContributors(organization, repository)
+                contributors <- retrieveContributors(organization, repository)
                 _            <- sAdd(repository, contributors.asJson.toString, contributors.map(_.asJson.toString): _*)
                 _            <- pExpire(repository, 1.minute)
               } yield contributors
             else
-              deserialize(response).orElseFail(GithubUnavailable)
+              response
+                .mapM(contributor => ZIO.fromEither(decode[Contributor](contributor)))
+                .orElseFail(GithubUnavailable)
           }.orElseFail(GithubUnavailable).provide(env)
 
-        private def deserialize(response: Chunk[String]): IO[Error, Chunk[Contributor]] =
-          response.mapM(contributor => ZIO.fromEither(decode[Contributor](contributor)))
-
-        private def getContributors(organization: String, repository: String): IO[ApiError, Chunk[Contributor]] =
-          SttpClient
-            .send(basicRequest.get(urlOf(organization, repository)).response(asJson[Chunk[Contributor]]))
-            .flatMap(_.body.fold(_ => ZIO.fail(UnknownProject), ZIO.succeed(_)))
-            .orElseFail(GithubUnavailable)
-            .provide(env)
-
-        private def urlOf(organization: String, repository: String): Uri =
-          uri"https://api.github.com/repos/$organization/$repository/contributors"
       }
     }
+
+  private[this] def retrieveContributors(
+    organization: String,
+    repository: String
+  ): ZIO[SttpClient, ApiError, Chunk[Contributor]] =
+    SttpClient
+      .send(basicRequest.get(urlOf(organization, repository)).response(asJson[Chunk[Contributor]]))
+      .flatMap(_.body.fold(_ => ZIO.fail(UnknownProject), ZIO.succeed(_)))
+      .orElseFail(GithubUnavailable)
+
+  private[this] def urlOf(organization: String, repository: String): Uri =
+    uri"https://api.github.com/repos/$organization/$repository/contributors"
+
 }
