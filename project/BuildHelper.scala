04c7d5a0e213d5e912e3cac3289d2af0968ef8f5
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2021-01-03 17:17:53 +0100

    Update zio-streams, zio-test, zio-test-sbt to 1.0.3+118-51b5bbe0-SNAPSHOT (#266)

--- project/BuildHelper.scala
+++ project/BuildHelper.scala
@@ -7,7 +7,7 @@ import scalafix.sbt.ScalafixPlugin.autoImport._
 object BuildHelper {
   val Scala212 = "2.12.12"
   val Scala213 = "2.13.3"
-  val Zio      = "1.0.3+90-84fbcdcb-SNAPSHOT"
+  val Zio      = "1.0.3+118-51b5bbe0-SNAPSHOT"
 
   def buildInfoSettings(packageName: String) =
     Seq(
