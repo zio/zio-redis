0bf79d46f2823dbdfd28c6ea28ed21739798187b
Dejan Mijic <dmijic@acm.org>
2021-06-14 22:56:40 +0200

    Enable 6.2 specific tests

--- redis/src/test/scala/zio/redis/StreamsSpec.scala
+++ redis/src/test/scala/zio/redis/StreamsSpec.scala
@@ -720,7 +720,6 @@ trait StreamsSpec extends BaseSpec {
           } yield assert(result)(isLeft(isSubtype[WrongType](anything)))
         }
       ),
-      // TODO: unignore when docker image for redis 6.2 comes out
       suite("xGroupCreateConsumer")(
         testM("new consumer") {
           for {
@@ -748,7 +747,7 @@ trait StreamsSpec extends BaseSpec {
             result    <- xGroupCreateConsumer[String, String, String](nonStream, group, consumer).either
           } yield assert(result)(isLeft)
         }
-      ) @@ ignore,
+      ),
       suite("xGroupDelConsumer")(
         testM("non-existing consumer") {
           for {
@@ -869,7 +868,6 @@ trait StreamsSpec extends BaseSpec {
             equalTo(PendingInfo(2L, Some(firstMsg), Some(lastMsg), Map(first -> 1L, second -> 1L)))
           )
         },
-        // TODO: unignore when redis docker image version 6.2 comes out
         testM("with 0ms idle time") {
           for {
             stream   <- uuid
@@ -880,7 +878,7 @@ trait StreamsSpec extends BaseSpec {
             _        <- xReadGroup[String, String, String, String, String, String](group, consumer)(stream -> ">")
             result   <- xPending(stream, group, 0.millis)
           } yield assert(result)(equalTo(PendingInfo(1L, Some(id), Some(id), Map(consumer -> 1L))))
-        } @@ ignore,
+        },
         testM("with 60s idle time") {
           for {
             stream   <- uuid
