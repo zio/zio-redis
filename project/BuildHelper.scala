561d0378fe8ff7270335be70380cabd372d13c40
Scala Steward <me@scala-steward.org>
2021-12-15 06:00:11 +0100

    Update zio-streams, zio-test, zio-test-sbt to 1.0.13

--- project/BuildHelper.scala
+++ project/BuildHelper.scala
@@ -22,7 +22,7 @@ object BuildHelper {
 
   val Scala212: String = versions("2.12")
   val Scala213: String = versions("2.13")
-  val Zio: String      = "1.0.12"
+  val Zio: String      = "1.0.13"
 
   def buildInfoSettings(packageName: String) =
     List(
