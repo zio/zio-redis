5483847888ffa31474706c770cd3d78a0ce8296c
Scala Steward <me@scala-steward.org>
2021-12-24 04:20:38 +0100

    Reformat with scalafmt 3.2.2

--- redis/src/test/scala/zio/redis/StringsSpec.scala
+++ redis/src/test/scala/zio/redis/StringsSpec.scala
@@ -12,11 +12,18 @@ import zio.test.environment.{TestClock, TestConsole, TestRandom, TestSystem}
 import zio.{Chunk, Has, ZIO}
 
 trait StringsSpec extends BaseSpec {
-  val stringsSuite: Spec[Has[Clock.Service] with Has[RedisExecutor.Service] with Has[TestClock.Service] with Has[
-    TestConsole.Service
-  ] with Has[TestRandom.Service] with Has[TestSystem.Service] with Has[RedisExecutor.Service] with Has[
-    Annotations.Service
-  ], TestFailure[RedisError], TestSuccess] =
+  val stringsSuite: Spec[Has[Clock.Service]
+    with Has[RedisExecutor.Service]
+    with Has[TestClock.Service]
+    with Has[
+      TestConsole.Service
+    ]
+    with Has[TestRandom.Service]
+    with Has[TestSystem.Service]
+    with Has[RedisExecutor.Service]
+    with Has[
+      Annotations.Service
+    ], TestFailure[RedisError], TestSuccess] =
     suite("strings")(
       suite("append")(
         testM("to the end of non-empty string") {
