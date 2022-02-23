80ed2502316612329c5270962ba5f008f24aafb8
Dejan Mijić <dmijic@acm.org>
2022-02-23 15:46:54 +0100

    Simplify benchmark framework (#555)

---
+++ benchmarks/src/main/scala/zio/redis/benchmarks/hash/HExistsBenchmarks.scala
@@ -0,0 +1,74 @@
+/*
+ * Copyright 2021 John A. De Goes and the ZIO contributors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package zio.redis.benchmarks.hash
+
+import java.util.concurrent.TimeUnit
+
+import org.openjdk.jmh.annotations._
+
+import zio.ZIO
+import zio.redis._
+import zio.redis.benchmarks._
+
+@State(Scope.Thread)
+@BenchmarkMode(Array(Mode.Throughput))
+@OutputTimeUnit(TimeUnit.SECONDS)
+@Measurement(iterations = 15)
+@Warmup(iterations = 15)
+@Fork(2)
+class HExistsBenchmarks extends BenchmarkRuntime {
+  @Param(Array("500"))
+  var size: Int = _
+
+  private var items: List[(String, String)] = _
+
+  private val key = "test-hash"
+
+  @Setup(Level.Trial)
+  def setup(): Unit = {
+    items = (0 to size).map(e => e.toString -> e.toString).toList
+    execute(hSet(key, items.head, items.tail: _*).unit)
+  }
+
+  @Benchmark
+  def laserdisc(): Unit = {
+    import _root_.laserdisc.fs2._
+    import _root_.laserdisc.{all => cmd, _}
+    import cats.implicits.toFoldableOps
+    import cats.instances.list._
+    execute[LaserDiscClient](c =>
+      items.traverse_(it => c.send(cmd.hexists(Key.unsafeFrom(key), Key.unsafeFrom(it._1))))
+    )
+  }
+
+  @Benchmark
+  def rediculous(): Unit = {
+    import cats.implicits._
+    import io.chrisdavenport.rediculous._
+    execute[RediculousClient](c => items.traverse_(it => RedisCommands.hexists[RedisIO](key, it._1).run(c)))
+  }
+
+  @Benchmark
+  def redis4cats(): Unit = {
+    import cats.instances.list._
+    import cats.syntax.foldable._
+    execute[Redis4CatsClient[String]](c => items.traverse_(it => c.hExists(key, it._1)))
+  }
+
+  @Benchmark
+  def zio(): Unit = execute(ZIO.foreach_(items)(it => hExists(key, it._1)))
+}
---
+++ benchmarks/src/main/scala/zio/redis/benchmarks/sets/SMembersBenchmarks.scala
@@ -0,0 +1,73 @@
+/*
+ * Copyright 2021 John A. De Goes and the ZIO contributors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package zio.redis.benchmarks.sets
+
+import java.util.concurrent.TimeUnit
+
+import org.openjdk.jmh.annotations._
+
+import zio.ZIO
+import zio.redis._
+import zio.redis.benchmarks._
+
+@State(Scope.Thread)
+@BenchmarkMode(Array(Mode.Throughput))
+@OutputTimeUnit(TimeUnit.SECONDS)
+@Measurement(iterations = 15)
+@Warmup(iterations = 15)
+@Fork(2)
+class SMembersBenchmarks extends BenchmarkRuntime {
+  @Param(Array("500"))
+  var count: Int = _
+
+  private var items: List[String] = _
+
+  private val key = "test-set"
+
+  @Setup(Level.Trial)
+  def setup(): Unit = {
+    items = (0 to count).toList.map(_.toString)
+    execute(sAdd(key, items.head, items.tail: _*).unit)
+  }
+
+  @Benchmark
+  def laserdisc(): Unit = {
+    import _root_.laserdisc.{all => cmd, _}
+    import _root_.laserdisc.fs2._
+    import cats.instances.list._
+    import cats.syntax.foldable._
+
+    execute[LaserDiscClient](c => items.traverse_(_ => c.send(cmd.smembers(Key.unsafeFrom(key)))))
+  }
+
+  @Benchmark
+  def rediculous(): Unit = {
+    import cats.implicits._
+    import io.chrisdavenport.rediculous._
+    execute[RediculousClient](c => items.traverse_(_ => RedisCommands.smembers[RedisIO](key).run(c)))
+  }
+
+  @Benchmark
+  def redis4cats(): Unit = {
+    import cats.instances.list._
+    import cats.syntax.foldable._
+    execute[Redis4CatsClient[String]](c => items.traverse_(_ => c.sMembers(key)))
+  }
+
+  @Benchmark
+  def zio(): Unit = execute(ZIO.foreach_(items)(_ => sMembers(key).returning[String]))
+}
