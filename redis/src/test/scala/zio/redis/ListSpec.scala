fd4ff782a2734993aaf20b3c0b92cbd11c0e333f
梦境迷离 <dreamylost@outlook.com>
2022-05-27 16:29:04 +0800

    fix

--- redis/src/test/scala/zio/redis/ListSpec.scala
+++ redis/src/test/scala/zio/redis/ListSpec.scala
@@ -9,7 +9,7 @@ import zio.{Chunk, _}
 import java.util.concurrent.TimeUnit
 
 trait ListSpec extends BaseSpec {
-  val listSuite: Spec[Redis, scala.Serializable] =
+  val listSuite: Spec[Redis, java.io.Serializable] =
     suite("lists")(
       suite("pop")(
         test("lPop non-empty list") {
