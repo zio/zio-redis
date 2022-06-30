eba640e292565a276733e9e7870a9c031239c0ce
梦境迷离 <dreamylost@outlook.com>
2022-06-30 21:06:05 +0800

    revert

--- project/BuildHelper.scala
+++ project/BuildHelper.scala
@@ -23,7 +23,7 @@ object BuildHelper {
 
   val Scala212: String = versions("2.12")
   val Scala213: String = versions("2.13")
-  val Zio: String      = "2.0.0"
+  val Zio: String      = "2.0.0-RC6"
 
   def buildInfoSettings(packageName: String) =
     List(
