c2addb9e6a861ca406c06d74aaced9acc48013f6
Akie Mejia <rfmejia@gmail.com>
2022-10-30 18:56:21 -0700

    Fix formatting issue

--- redis/src/main/scala/zio/redis/api/Scripting.scala
+++ redis/src/main/scala/zio/redis/api/Scripting.scala
@@ -78,10 +78,8 @@ trait Scripting {
    * Set the debug mode for subsequent scripts executed with [[zio.redis.api.Scripting.eval]].
    *
    * @param debugMode
-   *   - [[zio.redis.DebugMode.Yes]]: Enable non-blocking asynchronous debugging of Lua scripts (changes are
-   *     discarded).
-   *   - [[zio.redis.DebugMode.Sync]]: Enable blocking synchronous debugging of Lua scripts (saves changes to
-   *     data).
+   *   - [[zio.redis.DebugMode.Yes]]: Enable non-blocking asynchronous debugging of Lua scripts (changes are discarded).
+   *   - [[zio.redis.DebugMode.Sync]]: Enable blocking synchronous debugging of Lua scripts (saves changes to data).
    *   - [[zio.redis.DebugMode.No]]: Disables scripts debug mode.
    * @return
    *   unit if successful, error otherwise.
