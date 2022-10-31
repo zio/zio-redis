d043d2c7f65948b22627f1568d6aba17e58522dd
Anatoly Sergeev <twisttolya@gmail.com>
2022-10-31 19:03:37 +0300

    fix banchmark 2.12.16 compilation

--- benchmarks/src/main/scala/zio/redis/benchmarks/package.scala
+++ benchmarks/src/main/scala/zio/redis/benchmarks/package.scala
@@ -2,11 +2,11 @@ package zio.redis
 
 import cats.effect.{IO => CIO}
 import dev.profunktor.redis4cats.RedisCommands
-import io.chrisdavenport.rediculous.RedisConnection
+import io.chrisdavenport.rediculous
 import laserdisc.fs2.RedisClient
 
 package object benchmarks {
   type Redis4CatsClient[A] = RedisCommands[CIO, String, A]
   type LaserDiscClient     = RedisClient[CIO]
-  type RediculousClient    = RedisConnection[CIO]
+  type RediculousClient    = rediculous.RedisConnection[CIO]
 }
