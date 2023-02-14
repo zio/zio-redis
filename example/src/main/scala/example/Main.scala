f44c7fc28b2f02a48d4f5a84ca8f4ae51fb7856d
0pg <jahun1105@naver.com>
2023-02-15 02:14:31 +0900

    Add pubsub layer in Main

--- example/src/main/scala/example/Main.scala
+++ example/src/main/scala/example/Main.scala
@@ -21,7 +21,7 @@ import example.config.AppConfig
 import sttp.client3.httpclient.zio.HttpClientZioBackend
 import zhttp.service.Server
 import zio._
-import zio.redis.{Redis, RedisExecutor}
+import zio.redis.{Redis, RedisExecutor, RedisPubSub}
 import zio.schema.codec.{BinaryCodec, ProtobufCodec}
 
 object Main extends ZIOAppDefault {
@@ -33,6 +33,7 @@ object Main extends ZIOAppDefault {
         ContributorsCache.layer,
         HttpClientZioBackend.layer(),
         RedisExecutor.layer,
+        RedisPubSub.layer,
         Redis.layer,
         ZLayer.succeed[BinaryCodec](ProtobufCodec)
       )
