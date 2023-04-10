26560ca11331a3f8e015d8ce4394bca0936179fe
Dragutin Marjanović <dmarjanovic94@gmail.com>
2023-04-10 20:42:49 +0200

    Change return type of CLIENT INFO and CLIENT LIST (#813)
    * Remove unused class
    
    * Parse responses to ClientInfo
    
    * Improve docs
    
    * Address code remarks
    
    * Add 'from(char)' method to ClientFlag object
    
    * Add 'final' modifier
    
    * Make parsing ClientInfo safe
    
    * Use scala-collection-compat
    
    * Format code

--- modules/redis/src/main/scala/zio/redis/options/Connection.scala
+++ modules/redis/src/main/scala/zio/redis/options/Connection.scala
@@ -16,13 +16,22 @@
 
 package zio.redis.options
 
-import zio.Duration
+import zio.{Chunk, Duration}
 
 import java.net.InetAddress
+import scala.collection.compat._
 
 trait Connection {
   sealed case class Address(ip: InetAddress, port: Int) {
     private[redis] final def asString: String = s"${ip.getHostAddress}:$port"
+  }
+
+  object Address {
+    private[redis] final def fromString(addr: String): Option[Address] =
+      addr.split(":").toList match {
+        case ip :: port :: Nil => port.toIntOption.map(new Address(InetAddress.getByName(ip), _))
+        case _                 => None
+      }
   }
 
   sealed case class ClientEvents(readable: Boolean = false, writable: Boolean = false)
@@ -30,39 +39,44 @@ trait Connection {
   sealed trait ClientFlag
 
   object ClientFlag {
-    case object ToBeClosedAsap extends ClientFlag
-
-    case object Blocked extends ClientFlag
-
-    case object ToBeClosedAfterReply extends ClientFlag
-
-    case object WatchedKeysModified extends ClientFlag
-
-    case object IsMaster extends ClientFlag
-
-    case object MonitorMode extends ClientFlag
-
-    case object PubSub extends ClientFlag
-
-    case object ReadOnlyMode extends ClientFlag
-
-    case object Replica extends ClientFlag
-
-    case object Unblocked extends ClientFlag
-
-    case object UnixDomainSocket extends ClientFlag
-
-    case object MultiExecContext extends ClientFlag
-
-    case object KeysTrackingEnabled extends ClientFlag
-
+    case object Blocked                     extends ClientFlag
+    case object BroadcastTrackingMode       extends ClientFlag
+    case object IsMaster                    extends ClientFlag
+    case object KeysTrackingEnabled         extends ClientFlag
+    case object MonitorMode                 extends ClientFlag
+    case object MultiExecContext            extends ClientFlag
+    case object PubSub                      extends ClientFlag
+    case object ReadOnlyMode                extends ClientFlag
+    case object Replica                     extends ClientFlag
+    case object ToBeClosedAfterReply        extends ClientFlag
+    case object ToBeClosedAsap              extends ClientFlag
     case object TrackingTargetClientInvalid extends ClientFlag
+    case object Unblocked                   extends ClientFlag
+    case object UnixDomainSocket            extends ClientFlag
+    case object WatchedKeysModified         extends ClientFlag
 
-    case object BroadcastTrackingMode extends ClientFlag
+    private[redis] lazy val Flags =
+      Map(
+        'A' -> ClientFlag.ToBeClosedAsap,
+        'b' -> ClientFlag.Blocked,
+        'B' -> ClientFlag.BroadcastTrackingMode,
+        'c' -> ClientFlag.ToBeClosedAfterReply,
+        'd' -> ClientFlag.WatchedKeysModified,
+        'M' -> ClientFlag.IsMaster,
+        'O' -> ClientFlag.MonitorMode,
+        'P' -> ClientFlag.PubSub,
+        'r' -> ClientFlag.ReadOnlyMode,
+        'R' -> ClientFlag.TrackingTargetClientInvalid,
+        'S' -> ClientFlag.Replica,
+        't' -> ClientFlag.KeysTrackingEnabled,
+        'u' -> ClientFlag.Unblocked,
+        'U' -> ClientFlag.UnixDomainSocket,
+        'x' -> ClientFlag.MultiExecContext
+      )
   }
 
   sealed case class ClientInfo(
-    id: Long,
+    id: Option[Long] = None,
     name: Option[String] = None,
     address: Option[Address] = None,
     localAddress: Option[Address] = None,
@@ -71,9 +85,9 @@ trait Connection {
     idle: Option[Duration] = None,
     flags: Set[ClientFlag] = Set.empty,
     databaseId: Option[Long] = None,
-    subscriptions: Int = 0,
-    patternSubscriptions: Int = 0,
-    multiCommands: Int = 0,
+    subscriptions: Option[Int] = None,
+    patternSubscriptions: Option[Int] = None,
+    multiCommands: Option[Int] = None,
     queryBufferLength: Option[Int] = None,
     queryBufferFree: Option[Int] = None,
     outputBufferLength: Option[Int] = None,
@@ -86,6 +100,42 @@ trait Connection {
     redirectionClientId: Option[Long] = None,
     user: Option[String] = None
   )
+
+  object ClientInfo {
+    private[redis] final def from(line: String): ClientInfo = {
+      val data   = line.trim.split(" ").map(_.split("=").toList).collect { case k :: v :: Nil => k -> v }.toMap
+      val events = data.get("events")
+      new ClientInfo(
+        id = data.get("id").flatMap(_.toLongOption),
+        name = data.get("name"),
+        address = data.get("addr").flatMap(Address.fromString),
+        localAddress = data.get("laddr").flatMap(Address.fromString),
+        fileDescriptor = data.get("fd").flatMap(_.toLongOption),
+        age = data.get("age").flatMap(_.toLongOption).map(Duration.fromSeconds),
+        idle = data.get("idle").flatMap(_.toLongOption).map(Duration.fromSeconds),
+        flags = data
+          .get("flags")
+          .fold(Set.empty[ClientFlag])(_.foldLeft(Set.empty[ClientFlag])((fs, f) => fs ++ ClientFlag.Flags.get(f))),
+        databaseId = data.get("id").flatMap(_.toLongOption),
+        subscriptions = data.get("sub").flatMap(_.toIntOption),
+        patternSubscriptions = data.get("psub").flatMap(_.toIntOption),
+        multiCommands = data.get("multi").flatMap(_.toIntOption),
+        queryBufferLength = data.get("qbuf").flatMap(_.toIntOption),
+        queryBufferFree = data.get("qbuf-free").flatMap(_.toIntOption),
+        outputListLength = data.get("oll").flatMap(_.toIntOption),
+        outputBufferMem = data.get("omem").flatMap(_.toLongOption),
+        events = ClientEvents(readable = events.exists(_.contains("r")), writable = events.exists(_.contains("w"))),
+        lastCommand = data.get("cmd"),
+        argvMemory = data.get("argv-mem").flatMap(_.toLongOption),
+        totalMemory = data.get("total-mem").flatMap(_.toLongOption),
+        redirectionClientId = data.get("redir").flatMap(_.toLongOption),
+        user = data.get("user")
+      )
+    }
+
+    private[redis] final def from(lines: Array[String]): Chunk[ClientInfo] =
+      Chunk.fromArray(lines.map(from))
+  }
 
   sealed trait ClientKillFilter
 
