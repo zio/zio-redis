149ae4bc800dad23d9379c9768f9be967ff6084f
0pg <jahun1105@naver.com>
2023-04-27 17:39:18 +0900

    Remove unused code

--- modules/redis/src/main/scala/zio/redis/RedisError.scala
+++ modules/redis/src/main/scala/zio/redis/RedisError.scala
@@ -46,7 +46,6 @@ object RedisError {
   object Moved {
     def apply(slotAndAddress: (Slot, RedisUri)): Moved = Moved(slotAndAddress._1, slotAndAddress._2)
   }
-  final case class NoPubSubStream(key: String)     extends RedisError
   final case class IOError(exception: IOException) extends RedisError
   final case class CommandNameNotFound(message: String) extends RedisError
   sealed trait PubSubError extends RedisError
