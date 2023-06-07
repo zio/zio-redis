4a5092ac2a7ae7e0a6eb3d965b493ca3e9849d73
Scala Steward <scala_steward@virtuslab.com>
2023-06-07 05:22:56 +0000

    Update zio-schema, zio-schema-protobuf to 0.4.12

--- project/BuildHelper.scala
+++ project/BuildHelper.scala
@@ -31,7 +31,7 @@ object BuildHelper {
   val Scala3   = versions("3")
 
   val zioVersion       = "2.0.14"
-  val zioSchemaVersion = "0.4.11"
+  val zioSchemaVersion = "0.4.12"
 
   def buildInfoSettings(packageName: String) =
     List(
