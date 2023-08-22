1184dd0f99c629783d497550a9919ec7dfecd3e0
Dejan Mijic <dmijic@acm.org>
2023-08-22 15:50:13 +0200

    Ignore scheduler inference warnings

--- modules/redis/src/main/scala/zio/redis/internal/ClusterExecutor.scala
+++ modules/redis/src/main/scala/zio/redis/internal/ClusterExecutor.scala
@@ -85,6 +85,7 @@ private[redis] final class ClusterExecutor private (
       } yield cluster
     }
 
+  @annotation.nowarn
   private val retryPolicy: Schedule[Any, Throwable, (Duration, Long, Throwable)] =
     Schedule.exponential(config.retry.base, config.retry.factor) &&
       Schedule.recurs(config.retry.maxRecurs) &&
