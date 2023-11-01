b1553231d2f5252b923b00811b4fbe6e4410c873
Dejan Mijic <dmijic@acm.org>
2023-11-01 10:16:31 +0100

    Further decrease retries

--- modules/redis-it/src/test/scala/zio/redis/ApiSpec.scala
+++ modules/redis-it/src/test/scala/zio/redis/ApiSpec.scala
@@ -49,7 +49,7 @@ object ApiSpec
       masterNodeConfig,
       ZLayer.succeed(ProtobufCodecSupplier)
     ).filterNotTags(_.contains(BaseSpec.ClusterExecutorUnsupported))
-      .getOrElse(Spec.empty) @@ flaky(250)
+      .getOrElse(Spec.empty) @@ flaky(150)
 
   private final val SingleNodeSuite =
     suite("Single node executor")(
