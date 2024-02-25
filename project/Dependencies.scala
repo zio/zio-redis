4aac5f4d270297f0f39e574c93851596332fe10f
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2024-02-25 23:21:32 +0100

    Update zio, zio-json to 3.9.3 (#934)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -5,7 +5,7 @@ object Dependencies {
     val CatsEffect        = "3.5.3"
     val EmbeddedRedis     = "0.6"
     val Redis4Cats        = "1.5.2"
-    val Sttp              = "3.9.2"
+    val Sttp              = "3.9.3"
     val TlsChannel        = "0.8.1"
     val ZHttp             = "2.0.0-RC11"
     val ZioConfig         = "4.0.1"
