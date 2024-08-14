e781c5e128c94e32b4909ea82d2b9438a15f03f3
Scala Steward <scala_steward@virtuslab.com>
2024-08-14 10:37:39 +0000

    Update zio-schema, zio-schema-protobuf to 1.3.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -10,7 +10,7 @@ object Dependencies {
     val ZHttp             = "2.0.0-RC11"
     val ZioConfig         = "4.0.2"
     val ZioJson           = "0.6.2"
-    val ZioSchema         = "1.2.2"
+    val ZioSchema         = "1.3.0"
     val ZioTestContainers = "0.5.0"
   }
 
